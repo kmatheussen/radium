a952b99f555ad23c719c5d712d9d10fd35dd3ff4
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-07-02 14:45:54 +0200

    Forgot new files in previous commit

---
+++ midi/rtmidi/midi_rtmidi.cpp
@@ -0,0 +1,245 @@
+/* Copyright 2012 Kjetil S. Matheussen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
+
+
+#include <assert.h>
+
+#include "rtmidi-2.0.0/RtMidi.h"
+
+#include "../../common/nsmtracker.h"
+#include "../../common/memory_proc.h"
+#include "../OS_midi_proc.h"
+
+// (The *PutMidi* API needs to be cleaned up)
+static int midi_msg_len(int m1){
+  if((m1 & 0xf0) == 0xb0)
+    return 2;
+
+  if((m1 & 0xf0) == 0xc0)
+    return 2;
+
+  if(m1>=0xf0){
+    switch(m1){
+    case 0xf0:
+      return 0;
+    case 0xf1:
+      return 2;
+    case 0xf2:
+      return 3;
+    case 0xf3:
+      return 2;
+    case 0xf4:
+      return 0;
+    case 0xf5:
+      return 2;
+    case 0xf6:
+      return 1;
+    case 0xf7:
+      return 0;
+    default:
+      return 1;
+    }
+  }
+
+  return 3;
+}
+
+
+static std::vector<unsigned char> message1;
+static std::vector<unsigned char> message2;
+static std::vector<unsigned char> message3;
+
+
+void PutMidi(MidiPortOs port,
+             uint32_t msg
+             )
+{
+  if(port==NULL)
+    return;
+
+  RtMidiOut *midiout = static_cast<RtMidiOut*>(port);
+
+  unsigned int d3=(msg>>8)&0xff;
+  unsigned int d2=(msg>>16)&0xff;
+  unsigned int d1=(msg>>24)&0xff;
+
+  int len = midi_msg_len(d1);
+  if(len==0)
+    return;
+
+  if(len==1){
+    message1[0]=d1;
+    midiout->sendMessage(&message1);
+    return;
+  }
+
+  if(len==2){
+    message2[0]=d1;
+    message2[1]=d2;
+    midiout->sendMessage(&message2);
+    return;
+  }
+
+  message3[0]=d1;
+  message3[1]=d2;
+  message3[2]=d3;
+  midiout->sendMessage(&message3);
+}
+
+
+void GoodPutMidi(MidiPortOs port,
+                 uint32_t msg,
+                 uint32_t maxbuff
+                 )
+{
+  PutMidi(port,msg);
+}
+
+
+static const std::vector<std::string> get_port_names(RtMidi &rtmidi){
+  std::vector<std::string> ret;
+  unsigned int nPorts = rtmidi.getPortCount();
+
+  for (unsigned int i=0; i<nPorts; i++ ) {
+    std::string portName;
+    try {
+      portName = rtmidi.getPortName(i);
+    }
+    catch (RtError &error) {
+      RError(error.what());
+      continue;
+    }
+    std::cout << "  Output Port #" << i+1 << ": " << portName << '\n';
+    ret.push_back(portName);
+  }
+
+  return ret;
+}
+
+
+static char **vector_to_cstring_array(const std::vector<std::string> &strings, int *retval){
+  *retval = strings.size();
+  char **ret = (char**)talloc(sizeof(char*)*(*retval));
+
+  for(int i=0;i<*retval;i++)
+    ret[i] = talloc_strdup((char*)strings[i].c_str());
+
+  return ret;
+}
+
+
+static char **get_port_names(bool use_input_ports, int *retval){
+  std::vector<std::string> ret;
+
+  std::vector<RtMidi::Api> apis;
+  RtMidi::getCompiledApi(apis);
+
+  for(unsigned int i=0;i<apis.size();i++){
+    try{
+      std::vector<std::string> port_names;
+      if(use_input_ports==true){
+        RtMidiIn rtmidi(apis[i]);
+        port_names = get_port_names(rtmidi);
+      }else{
+        RtMidiOut rtmidi(apis[i]);
+        port_names = get_port_names(rtmidi);
+      }
+      ret.insert(ret.end(), port_names.begin(), port_names.end());
+    }catch ( RtError &error ) {
+      RError(error.what());
+    }
+  }
+
+  return vector_to_cstring_array(ret, retval);
+}
+
+
+char **MIDI_getInputPortNames(int *retsize){
+  return get_port_names(true, retsize);
+}
+
+
+char **MIDI_getOutputPortNames(int *retsize){
+  return get_port_names(false, retsize);
+}
+
+
+MidiPortOs MIDI_getMidiPortOs(char *name){
+  std::vector<RtMidi::Api> apis;
+  RtMidi::getCompiledApi(apis);
+  RtMidi::Api api;
+  int portnum = -1;
+
+  for(unsigned int i1=0;i1<apis.size();i1++){
+    try{
+      RtMidiOut rtmidi(apis[i1]);
+      std::vector<std::string> port_names = get_port_names(rtmidi);
+
+      for(unsigned int i2=0;i2<port_names.size();i2++)
+        if(port_names[i2] == std::string(name)){
+          api = apis[i1];
+          portnum = i2;
+          goto gotit;
+        }
+    }catch ( RtError &error ) {
+      RError(error.what());
+    }
+  }
+
+  {
+    RtMidiOut *ret;
+    try{
+      ret = new RtMidiOut();
+      ret->openVirtualPort();
+    }catch ( RtError &error ) {
+      RError(error.what());
+      return NULL;
+    }
+    return ret;
+  }
+
+ gotit:
+  RtMidiOut *ret;
+  try{
+    ret = new RtMidiOut(api);
+    ret->openPort(portnum);
+  }catch ( RtError &error ) {
+    RError(error.what()); // Can't get this exception to work if provocing wrong arguments above. (tried -fexceptions)
+    return NULL;
+  }
+  printf("opened. api %p: %d, portnum: %d\n",ret,(int)api,(int)portnum);
+
+  return ret;
+}
+
+
+bool MIDI_New(struct Instruments *instrument){
+  message1.push_back(0);
+
+  message2.push_back(0);
+  message2.push_back(0);
+
+  message3.push_back(0);
+  message3.push_back(0);
+  message3.push_back(0);
+
+  return true;
+}
+
+
+void MIDI_Delete(void){
+}
+
