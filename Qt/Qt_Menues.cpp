6ed7a3d771a48c8ba14c0c914277c8ab3e105833
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-07-23 11:41:26 +0200

    Qt: Moved menu handling into Qt_Menues.cpp

---
+++ Qt/Qt_Menues.cpp
@@ -0,0 +1,135 @@
+/* Copyright 2012 Kjetil S. Matheussen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
+
+#include "Python.h"
+
+#ifdef USE_QT4
+#  include <Q3PopupMenu>
+#else
+#  include "qpopupmenu.h"
+#endif
+
+#include "../common/nsmtracker.h"
+#include "../common/gfx_proc.h"
+#include "MyWidget.h"
+
+#include "Qt_Menues_proc.h"
+
+extern bool doquit;
+extern struct Root *root;
+extern QApplication *qapplication;
+
+
+
+struct Menues{
+  struct Menues *up;
+  Q3PopupMenu *menu;
+  QMenuBar *base;
+};
+
+static struct Menues *current_menu = NULL;
+
+class MenuItem : public QWidget
+{
+    Q_OBJECT
+public:
+  MenuItem(const char *name, const char *python_command, Q3PopupMenu *menu = NULL, bool checkable = false, int checkval = 0){
+    this->python_command = strdup(python_command);
+    this->checkable = checkable;
+    this->checkval = checkval;
+
+    if(menu!=NULL) {
+      if(current_menu->base!=NULL){
+        int id = current_menu->base->insertItem(name, menu);
+        current_menu->base->connectItem(id, this, SLOT(clicked()));
+      }else{
+        int id = current_menu->menu->insertItem(name, menu);
+        current_menu->menu->connectItem(id, this, SLOT(clicked()));
+      }
+    }else{
+      if(current_menu->base!=NULL){
+        current_menu->base->insertItem(name, this, SLOT(clicked()));
+      }else{
+        int id = current_menu->menu->insertItem(name, this, SLOT(clicked()));
+        if(checkable==true)
+          current_menu->menu->setItemChecked(id, checkval);
+      }
+    }
+  }
+
+private:
+  const char *python_command;
+  bool checkable;
+  int checkval;
+
+public slots:
+  void clicked() {
+    struct Tracker_Windows *window = root->song->tracker_windows;
+    if(checkable==true){
+      char temp[500];
+      this->checkval = this->checkval==1?0:1; // switch value.
+      sprintf(temp,python_command,checkval?"1":"0");
+      DO_GFX(PyRun_SimpleString(temp));
+    }else
+      DO_GFX(PyRun_SimpleString(python_command));
+    //static_cast<MyWidget*>(window->os_visual.widget)->update();
+    static_cast<MyWidget*>(window->os_visual.widget)->repaint(); // Why isn't calling update() enough?
+    if(doquit==true)
+      qapplication->quit();
+  }
+};
+
+#include "mQt_Menues.cpp"
+
+
+void GFX_AddMenuItem(struct Tracker_Windows *tvisual, const char *name, const char *python_command){
+  new MenuItem(name, python_command);
+}
+
+void GFX_AddCheckableMenuItem(struct Tracker_Windows *tvisual, const char *name, const char *python_command, int checkval){
+  new MenuItem(name, python_command, NULL, true, checkval);
+}
+
+void GFX_AddMenuSeparator(struct Tracker_Windows *tvisual){
+  if(current_menu->menu==NULL){
+    RError("Can not add separator at toplevel");
+    return;
+  }
+  current_menu->menu->insertSeparator();
+}
+
+void GFX_AddMenuMenu(struct Tracker_Windows *tvisual, const char *name, const char *command){
+  struct Menues *menu = (struct Menues*)calloc(1, sizeof(struct Menues));
+  menu->up = current_menu;
+  menu->menu = new Q3PopupMenu();
+  new MenuItem(name, command, menu->menu);
+  current_menu = menu;
+}
+
+void GFX_GoPreviousMenuLevel(struct Tracker_Windows *tvisual){
+  if(current_menu->base!=NULL){
+    RError("Already at top level");
+    return;
+  }
+  current_menu = current_menu->up;
+}
+
+
+void initMenues(QMenuBar *base_menu){
+  current_menu = (struct Menues*)calloc(1, sizeof(struct Menues));
+  current_menu->base = base_menu;
+}
+
--- Qt/Qt_visual.cpp
+++ Qt/Qt_visual.cpp
@@ -14,14 +14,14 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
-#include "Python.h"
-
-
 #include <stdbool.h>
+#include <math.h>
+
 #include "MyWidget.h"
 #include "Qt_Bs_edit_proc.h"
 #include "Qt_instruments_proc.h"
 #include "Qt_colors_proc.h"
+#include "Qt_Menues_proc.h"
 
 #include <qpainter.h>
 #include <qstatusbar.h>
@@ -34,9 +34,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 //#include <QMainWindow>
 #  include <Q3PointArray>
 #  include <QPixmap>
-#  include <Q3PopupMenu>
-#else
-#  include "qpopupmenu.h"
 #endif
 
 #include <unistd.h>
@@ -136,108 +133,6 @@ MyWidget::~MyWidget()
     delete[] points;				// cleanup
     delete[] colors;
     */
-}
-
-extern struct Root *root;
-extern QApplication *qapplication;
-extern bool doquit;
-
-struct Menues{
-  struct Menues *up;
-  Q3PopupMenu *menu;
-  QMenuBar *base;
-};
-
-static struct Menues *current_menu = NULL;
-
-class MenuItem : public QWidget
-{
-    Q_OBJECT
-public:
-  MenuItem(const char *name, const char *python_command, Q3PopupMenu *menu = NULL, bool checkable = false, int checkval = 0){
-    this->python_command = strdup(python_command);
-    this->checkable = checkable;
-    this->checkval = checkval;
-
-    if(menu!=NULL) {
-      if(current_menu->base!=NULL){
-        int id = current_menu->base->insertItem(name, menu);
-        current_menu->base->connectItem(id, this, SLOT(clicked()));
-      }else{
-        int id = current_menu->menu->insertItem(name, menu);
-        current_menu->menu->connectItem(id, this, SLOT(clicked()));
-      }
-    }else{
-      if(current_menu->base!=NULL){
-        current_menu->base->insertItem(name, this, SLOT(clicked()));
-      }else{
-        int id = current_menu->menu->insertItem(name, this, SLOT(clicked()));
-        if(checkable==true)
-          current_menu->menu->setItemChecked(id, checkval);
-      }
-    }
-  }
-
-private:
-  const char *python_command;
-  bool checkable;
-  int checkval;
-
-public slots:
-  void clicked() {
-    struct Tracker_Windows *window = root->song->tracker_windows;
-    if(checkable==true){
-      char temp[500];
-      this->checkval = this->checkval==1?0:1; // switch value.
-      sprintf(temp,python_command,checkval?"1":"0");
-      DO_GFX(PyRun_SimpleString(temp));
-    }else
-      DO_GFX(PyRun_SimpleString(python_command));
-    //static_cast<MyWidget*>(window->os_visual.widget)->update();
-    static_cast<MyWidget*>(window->os_visual.widget)->repaint(); // Why isn't calling update() enough?
-    if(doquit==true)
-      qapplication->quit();
-  }
-};
-
-#include "mQt_visual.cpp"
-
-void GFX_AddMenuItem(struct Tracker_Windows *tvisual, const char *name, const char *python_command){
-  new MenuItem(name, python_command);
-}
-
-void GFX_AddCheckableMenuItem(struct Tracker_Windows *tvisual, const char *name, const char *python_command, int checkval){
-  new MenuItem(name, python_command, NULL, true, checkval);
-}
-
-void GFX_AddMenuSeparator(struct Tracker_Windows *tvisual){
-  if(current_menu->menu==NULL){
-    RError("Can not add separator at toplevel");
-    return;
-  }
-  current_menu->menu->insertSeparator();
-}
-
-void GFX_AddMenuMenu(struct Tracker_Windows *tvisual, const char *name, const char *command){
-  struct Menues *menu = (struct Menues*)calloc(1, sizeof(struct Menues));
-  menu->up = current_menu;
-  menu->menu = new Q3PopupMenu();
-  new MenuItem(name, command, menu->menu);
-  current_menu = menu;
-}
-
-void GFX_GoPreviousMenuLevel(struct Tracker_Windows *tvisual){
-  if(current_menu->base!=NULL){
-    RError("Already at top level");
-    return;
-  }
-  current_menu = current_menu->up;
-}
-
-
-static void initMenues(QMenuBar *base_menu){
-  current_menu = (struct Menues*)calloc(1, sizeof(struct Menues));
-  current_menu->base = base_menu;
 }
 
 static void setFontValues(struct Tracker_Windows *tvisual, const QFont &font){
