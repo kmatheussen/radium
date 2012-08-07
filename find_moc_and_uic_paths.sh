95f13e25ffea4a4f6b7a48be9975e4584285f300
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-07 15:41:10 +0200

    build: make find_moc_and_uic_paths.sh work with ubuntu

--- find_moc_and_uic_paths.sh
+++ find_moc_and_uic_paths.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-if [ /etc/fedora-release ] ; then
+if [ -f /etc/fedora-release ] ; then
     if uname -a |grep x86_64 >/dev/null ; then
         PATH=/usr/lib64/qt-3.3/bin/
     else
         PATH=/usr/lib/qt-3.3/bin/
     fi
-elif [ /etc/ubuntu-release ] ; then
+
+elif grep -i ubuntu /etc/lsb-release >/dev/null ; then
     PATH=/usr/share/qt3/bin/
-elif [ /etc/debian-release ] ; then
+
+elif grep -i debian /etc/lsb-release >/dev/null ; then
     PATH=/usr/share/qt3/bin/
-elif [ /etc/arch-release ] ; then
+
+elif grep -i arch /etc/lsb-release >/dev/null ; then
     PATH=/opt/qt/bin/
+
 else
     PATH=
+
 fi
 
 echo $PATH$1
