000b1756e1a3d77871828d376bb4e8bfc1b91a7c
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-07 15:34:53 +0200

    build: make find_moc_and_uic_paths.sh work with 64 bit fedora

--- find_moc_and_uic_paths.sh
+++ find_moc_and_uic_paths.sh
@@ -1,35 +1,19 @@
 #!/bin/bash
 
-
-#distro finding script copied from http://legroom.net/2010/05/05/generic-method-determine-linux-or-unix-distribution-name
-
-# Determine OS platform
-UNAME=$(uname | tr "[:upper:]" "[:lower:]")
-# If Linux, try to determine specific distribution
-
-if [ "$UNAME" == "linux" ] ; then
-    # If available, use LSB to identify distribution
-    if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
-        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
-    # Otherwise, use release info file
+if [ /etc/fedora-release ] ; then
+    if uname -a |grep x86_64 >/dev/null ; then
+        PATH=/usr/lib64/qt-3.3/bin/
     else
-        export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+        PATH=/usr/lib/qt-3.3/bin/
     fi
-fi
-# For everything else (or if above failed), just use generic identifier
-[ "$DISTRO" == "" ] && export DISTRO=$UNAME
-unset UNAME
-
-#echo $DISTRO
-
-if [ $DISTRO = Ubuntu ] ; then
+elif [ /etc/ubuntu-release ] ; then
     PATH=/usr/share/qt3/bin/
-elif [ $DISTRO = Debian ] ; then
+elif [ /etc/debian-release ] ; then
     PATH=/usr/share/qt3/bin/
-elif [ $DISTRO = Fedora ] ; then
-    PATH=/usr/lib/qt-3.3/bin/
-elif [ $DISTRO = Arch ] ; then
+elif [ /etc/arch-release ] ; then
     PATH=/opt/qt/bin/
+else
+    PATH=
 fi
 
 echo $PATH$1
