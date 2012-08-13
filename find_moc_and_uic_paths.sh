44d1dd420cfe48e1bd7422db5951ee31c7b61492
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-13 12:25:45 +0200

    Build: Use first uic/moc in path, if V4

--- find_moc_and_uic_paths.sh
+++ find_moc_and_uic_paths.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if which $1 >/dev/null 2>/dev/null ; then
+    if $1 -v 2>&1 | grep Qt\ 4 >/dev/null ; then
+        echo $1
+        exit 0
+    elif $1 -v 2>&1 | grep version\ 4 >/dev/null ; then
+        echo $1
+        exit 0
+    fi
+fi
+
+
 if [ -f /etc/fedora-release ] ; then
     if uname -a |grep x86_64 >/dev/null ; then
         echo /usr/bin/$1-qt4
