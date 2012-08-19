3c56af9d2fbfa2cdef39748bc18a85cae04dc4cf
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-19 10:39:32 +0200

    build: fix

--- build_linux.sh
+++ build_linux.sh
@@ -4,8 +4,8 @@ export PYTHONEXE=`./find_python_path.sh`
 export MOC="`./find_moc_and_uic_paths.sh moc`"
 export UIC="`./find_moc_and_uic_paths.sh uic`"
 
-#export BUILDTYPE=RELEASE
-export BUILDTYPE=DEBUG
+export BUILDTYPE=RELEASE
+#export BUILDTYPE=DEBUG
 export OPTIMIZE="-mtune=native -O3"
 
 export CCC=g++
@@ -21,10 +21,10 @@ export GTK_LDFLAGS="`pkg-config --libs gtk+-2.0`"
 export QT_CFLAGS="`$PKG --cflags Qt3Support`"
 export QT_LDFLAGS="`$PKG --libs Qt3Support`"
 
-#export RTMIDI_CFLAGS="-D__UNIX_JACK__ -D__LINUX_ALSA__  -D__RTMIDI_DEBUG__"
-#export RTMIDI_LDFLAGS="-lpthread -lasound -ljack"
+export RTMIDI_CFLAGS="-D__UNIX_JACK__ -D__LINUX_ALSA__  -D__RTMIDI_DEBUG__"
+export RTMIDI_LDFLAGS="-lpthread -lasound -ljack"
 
-export OS_OPTS=-DMEMORY_DEBUG
+export OS_OPTS=
 
 PYTHONLIBPATH=`$PYTHONEXE -c "import sys;print '-L'+sys.prefix+'/lib'"`
 PYTHONLIBNAME=`$PYTHONEXE -c "import sys;print '-lpython'+sys.version[:3]"`
