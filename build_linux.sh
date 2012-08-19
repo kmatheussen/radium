94674e1d57ad23b419aee717cf14e3c448b97471
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-19 10:37:44 +0200

    build: Ubuntu 11 compilation fixes. (thanks to frank neumann)

--- build_linux.sh
+++ build_linux.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export PYTHONEXE=`which python2`
+export PYTHONEXE=`./find_python_path.sh`
 export MOC="`./find_moc_and_uic_paths.sh moc`"
 export UIC="`./find_moc_and_uic_paths.sh uic`"
 
-export BUILDTYPE=RELEASE
+#export BUILDTYPE=RELEASE
+export BUILDTYPE=DEBUG
 export OPTIMIZE="-mtune=native -O3"
 
 export CCC=g++
@@ -20,10 +21,10 @@ export GTK_LDFLAGS="`pkg-config --libs gtk+-2.0`"
 export QT_CFLAGS="`$PKG --cflags Qt3Support`"
 export QT_LDFLAGS="`$PKG --libs Qt3Support`"
 
-export RTMIDI_CFLAGS="-D__UNIX_JACK__ -D__LINUX_ALSA__  -D__RTMIDI_DEBUG__"
-export RTMIDI_LDFLAGS="-lpthread -lasound -ljack"
+#export RTMIDI_CFLAGS="-D__UNIX_JACK__ -D__LINUX_ALSA__  -D__RTMIDI_DEBUG__"
+#export RTMIDI_LDFLAGS="-lpthread -lasound -ljack"
 
-export OS_OPTS=
+export OS_OPTS=-DMEMORY_DEBUG
 
 PYTHONLIBPATH=`$PYTHONEXE -c "import sys;print '-L'+sys.prefix+'/lib'"`
 PYTHONLIBNAME=`$PYTHONEXE -c "import sys;print '-lpython'+sys.version[:3]"`
---
+++ find_python_path.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if which python >/dev/null 2>/dev/null ; then
+    which python2
+elif which python >/dev/null 2>/dev/null ; then
+    which python
+else
+    echo "<<<python_not_found>>>"
+fi
