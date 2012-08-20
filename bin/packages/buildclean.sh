3b577dc5f71e2346cdc61926cb18e8b3849ee5b4
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-20 11:25:09 +0200

    Remove setproctitle. Radium is a normal executable now

--- bin/packages/buildclean.sh
+++ bin/packages/buildclean.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-rm -fr bin include lib man share Python-2.2.2 libglade-0.17 pygtk-0.6.11 gc-7.2 xmessage-1.0.3 dvarrazzo-py-setproctitle-c35a1bf deletemetorebuild
-#rm -f /tmp/radium
+rm -fr bin include lib man share gc-7.2 xmessage-1.0.3 deletemetorebuild
