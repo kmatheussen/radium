e19d2aae824a4412f3c96ce4894afe030b20bef5
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-08-31 09:14:54 +0200

    fix build scripts a bit

--- build_darwinx_gtk_visual.sh
+++ build_darwinx_gtk_visual.sh
@@ -1,59 +1,15 @@
 #!/bin/sh
 
-export PYTHONEXE=python #In Mingw, this is just the python we use to autogenerate files and so forth during the build process, it is not used to find header or link files
-export MOC=moc-qt4
-export UIC=uic-qt4
+echo "Warning, embedding gtk widgets in qt on macosx didn't work very well,"
+echo "plus that it's so insanely complicated to distribute executable binaries which uses pango fonts (look at gimp and ardour for instance),"
+echo "that gtk for macosx has been disbanded."
+echo
+echo "This file is not maintained."
 
-export OPTIMIZE="-mtune=generic -O3"
-
-export CCC=darwinx-g++
-export CC=darwinx-gcc
-export LINKER=darwinx-g++
-
-export BUILDTYPE=DEBUG
-#export BUILDTYPE=RELEASE
-
-export PYPATH=/home/kjetil/radium-qt4/darwinx/Versions/Current/include/python2.7
-
-export GTK_CFLAGS="`darwinx-pkg-config --cflags gtk+-2.0`"
-export GTK_LDFLAGS="`darwinx-pkg-config --static --libs gtk+-2.0`"
-
-export QT_CFLAGS="`darwinx-pkg-config --cflags Qt3Support`"
-export QT_LDFLAGS="`darwinx-pkg-config --libs Qt3Support`"
-
-# MEMORY_DEBUG is defined since bdw-gc doesn't work properly under wine. (It does in real windows though.)
-export OS_OPTS="-DFOR_MACOSX -DMEMORY_DEBUG"
-export OS_LDFLAGS="cocoa_embed.o `pwd`/darwinx/gc-7.2/.libs/libgc.a `pwd`/darwinx/Versions/2.7/lib/python2.7/config/libpython2.7.a -lpthread"
-
-#export RTMIDI_CFLAGS="-D__WINDOWS_MM__ -D__WINDOWS_KS__ -D__RTMIDI_DEBUG__"
-#export RTMIDI_CFLAGS="-D__WINDOWS_MM__ -D__RTMIDI_DEBUG__"
-#export RTMIDI_LDFLAGS="-lwinmm /usr/i686-w64-mingw32/sys-root/mingw/lib/libksuser.a -lsetupapi"
-export RTMIDI_CFLAGS="-D__DUMMY__ -D__RTMIDI_DEBUG__"
-export RTMIDI_LDFLAGS=""
-
-if [ $BUILDTYPE = "RELEASE" ] ; then
-    export OS_LDFLAGS="$OS_LDFLAGS"
-fi
-
-export OBJ_X11=
-export OBJ_WIN=
-
-
-# To build gc for mingw32:
-#
-# cd bin/packages/gc-7.2
-# mingw32-configure
-# echo "void RADIUM_ensure_bin_packages_gc_is_used(void){}" >>malloc.c
-# mingw32-make
-# (that's it)
+export OS_LDFLAGS="cocoa_embed.o $OS_LDFLAGS"
 
 $CC -c -Wall $GTK_CFLAGS macosx/cocoa_embed.m
 
-darwinx-make radium $@
+./build_darwinx_common.sh $@
 
-cp bin/radium /home/kjetil/Dropbox/radium.app/Contents/MacOS/
-
-
-#cp bin/radium mingw/radium/radium.exe
-#cp bin/radium temp/bin/radium.exe
 
--- build_linux_qt_visual.sh
+++ build_linux_qt_visual.sh
@@ -1,38 +1,5 @@
 #!/bin/sh
 
-export PYTHONEXE=`./find_python_path.sh`
-export MOC="`./find_moc_and_uic_paths.sh moc`"
-export UIC="`./find_moc_and_uic_paths.sh uic`"
-
-#export BUILDTYPE=RELEASE
-export BUILDTYPE=DEBUG
-export OPTIMIZE="-mtune=native -O3"
-
-export CCC=g++
-export CC=gcc
-export LINKER=g++
-
-export PKG=pkg-config
-export PYPATH=`$PYTHONEXE -c "import sys;print sys.prefix+'/include/python'+sys.version[:3]"`
-
 export VISUAL="-DUSE_QT_VISUAL=1 -DUSE_GTK_VISUAL=0"
 
-export GTK_CFLAGS="`pkg-config --cflags gtk+-2.0`"
-export GTK_LDFLAGS="`pkg-config --libs gtk+-2.0`"
-
-export QT_CFLAGS="`$PKG --cflags Qt3Support`"
-export QT_LDFLAGS="`$PKG --libs Qt3Support`"
-
-export RTMIDI_CFLAGS="-D__UNIX_JACK__ -D__LINUX_ALSA__  -D__RTMIDI_DEBUG__"
-export RTMIDI_LDFLAGS="-lpthread -lasound -ljack"
-
-export OS_OPTS=
-
-PYTHONLIBPATH=`$PYTHONEXE -c "import sys;print '-L'+sys.prefix+'/lib'"`
-PYTHONLIBNAME=`$PYTHONEXE -c "import sys;print '-lpython'+sys.version[:3]"`
-export OS_LDFLAGS="-pthread -lrt -lX11 bin/packages/gc-7.2/.libs/libgc.a $PYTHONLIBPATH $PYTHONLIBNAME"
-
-export OBJ_WIN=""
-export OBJ_MACOSX=""
-
-make radium $@
+./build_linux_common.sh $@
