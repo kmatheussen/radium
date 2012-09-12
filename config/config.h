d3649e9811e94dbac3c96df71c1c0e0efb6f83dd
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-09-12 09:37:18 +0200

    Adjusted editor background color

--- config/config.h
+++ config/config.h
@@ -3,7 +3,7 @@
 #define VERSION 0.65
 #define VERSION_LETTER ""
 #define DISKVERSION 0.65
-#define SETTINGSVERSION 0.69
+#define SETTINGSVERSION 0.70
 
 
 /* Define this when profiling with sas/c. */
