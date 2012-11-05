1b020a703f8e2e3e325fba90fc7be9b4847f22b9
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-11-05 22:23:03 +0100

    add back fluidsynth and libgig packages again, but as tarballs this time

--- bin/packages/build.sh
+++ bin/packages/build.sh
@@ -66,17 +66,19 @@ cd xmessage-1.0.3
 make -j3
 cd ..
 
+tar xvzf fluidsynth-1.1.6.tar.gz
 cd fluidsynth-1.1.6
-#make clean
+make clean
 CFLAGS="-mtune=native -O3" CPPFLAGS="-mtune=native -O3" ./configure --enable-static --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-libsndfile-support --disable-portaudio-support --disable-oss-support --disable-midishare --disable-jack-support --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --disable-aufile-support --disable-dbus-support
 # --enable-debug
-make -j7
+make -j3
 cd ..
 
+tar xvzf libgig.tar.gz
 cd libgig
-#make clean
+make clean
 CFLAGS="-mtune=native -O3" CPPFLAGS="-mtune=native -O3" ./configure
-make -j7
+make -j3
 cd ..
 
 touch deletemetorebuild
