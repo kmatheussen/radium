c8546a9f21f8b344a17e2ddc7c043596754e4689
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-11-05 16:35:39 +0100

    Modified fluidsynth

---
+++ bin/packages/fluidsynth-1.1.6/configure.sh
@@ -0,0 +1,2 @@
+./configure --enable-static --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-libsndfile-support --disable-portaudio-support --disable-oss-support --disable-midishare --disable-jack-support --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --disable-aufile-support --enable-debug
+
---
+++ bin/packages/fluidsynth-1.1.6/make_diff.sh
@@ -0,0 +1,4 @@
+diff -ru src_org src|grep -v ^Binary\ files|grep -v ^Only\ in >fluid_const_sample_patch.diff
+
+diff -ru include_org include|grep -v ^Binary\ files|grep -v ^Only\ in >>fluid_const_sample_patch.diff
+
