2f442558afb602283efdef7326f4b10d67cf02a8
Kjetil Matheussen <k.s.matheussen@notam02.no>
2012-11-05 18:22:35 +0100

    Remove modified libgig and libfluidsynth as directories.

--- bin/packages/fluidsynth-1.1.6/configure.sh
+++
@@ -1,2 +0,0 @@
-./configure --enable-static --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-libsndfile-support --disable-portaudio-support --disable-oss-support --disable-midishare --disable-jack-support --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --disable-aufile-support --enable-debug
-
