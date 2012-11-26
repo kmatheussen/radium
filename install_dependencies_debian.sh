69221813ed8d4c366d7eafaf273ea6492321ddf9
kjetil <kjetil@kjetil-VirtualBox.(none)>
2012-11-26 19:47:17 +0100

    script to install debian/ubuntu/etc. dependencies for building

---
+++ install_dependencies_debian.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt-get -y install  libqt4-dev
+sudo apt-get -y install  qt4-dev-tools
+sudo apt-get -y install  libxaw7-dev
+sudo apt-get -y install  python2-dev
+sudo apt-get -y install python-dev
+sudo apt-get -y install  libasound2-dev
+sudo apt-get -y install  libjack-dev
+sudo apt-get -y install  libsamplerate-dev
+sudo apt-get -y install  liblrdf-dev
+sudo apt-get -y install  libsndfile-dev
+sudo apt-get -y install  ladspa-sdk
+sudo apt-get -y install  glib2-dev
+sudo apt-get -y install libglib2.0-dev
+sudo apt-get -y install wget
+sudo apt-get -y install gcc
+sudo apt-get -y install g++
+sudo apt-get -y install make
+sudo apt-get -y install bash
