8e3067538485f4133d10d3d7c1420c081dc872b9
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2014-01-20 16:46:41 +0100

    Forgotten file in prev. commit.

---
+++ bin/X11_xkb.py
@@ -0,0 +1,26 @@
+import subprocess,os
+
+global_layout = ""
+
+def get_xkb(setxkbmap):
+    process = subprocess.Popen([setxkbmap,"-query"], stdout=subprocess.PIPE)
+    output = process.communicate()
+    lines = output[0].split("\n")
+    matches = [x for x in lines if x[:len("layout:")]=="layout:"][0]
+    layout = matches.split(':')[1].strip()
+    print "keyboard layout: "+layout
+    return layout
+
+def set_xkb(setxkbmap,layout):
+    os.system(setxkbmap+" -layout "+layout)
+
+def save_xkb(setxkbmap):
+    global global_layout
+    global_layout = get_xkb(setxkbmap)
+
+def restore_xkb(setxkbmap):
+    global global_layout
+    set_xkb(setxkbmap, global_layout)
+
+if __name__=="__main__":
+    get_xkb("bin/packages/setxkbmap/setxkbmap")
