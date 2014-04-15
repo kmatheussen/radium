4163dd347643e243bb62e63bbbd513de5f679fcd
Kjetil Matheussen <k.s.matheussen@notam02.no>
2014-04-15 21:47:15 +0200

    mingw build fixes

--- audio/undo_pd_controllers.c
+++ audio/undo_pd_controllers.c
@@ -14,8 +14,6 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
-
-
 #include "../common/nsmtracker.h"
 #include "../common/undo.h"
 #include "../common/OS_Player_proc.h"
@@ -99,4 +97,3 @@ static void *Undo_Do_PdControllers(
 
   return undo_ae;
 }
-
