cb7aa299f50b84cc4b807423e33bceb4cdc3162e
Kjetil Matheussen <k.s.matheussen@notam02.no>
2014-10-25 10:51:47 +0200

    upgrade_vl.sh

---
+++ bin/packages/upgrade_vl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -fr Visualization-Library-master.tar.gz Visualization-Library-master
+wget https://github.com/kmatheussen/Visualization-Library/archive/master.tar.gz
+mv master.tar.gz Visualization-Library-master.tar.gz
