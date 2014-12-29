c33dfca0caf63615fe10de61972f1d405b2e9875
Kjetil Matheussen <k.s.matheussen@notam02.no>
2014-12-29 20:14:32 +0100

    Inform about config file changes

--- Qt/Qt_Main.cpp
+++ Qt/Qt_Main.cpp
@@ -717,12 +717,12 @@ int radium_main(char *arg){
 
   //printf("col: -%s-, font: -%s-\n",SETTINGS_read_string("last_color_version","0.0"),SETTINGS_read_string("last_system_font_version","0.0"));
 
-  if(strcmp(SETTINGS_read_string("last_color_version","0.0"),"3.0.b2.1")){
+  if(strcmp(SETTINGS_read_string("last_color_version","0.0"),"3.0.b2.2")){
     GFX_Message(NULL,
                 "Note!\n\n"
                 "The default colors have changed. In case you have run Radium before, you might want to go to the Edit menu and select \"Set Default Colors\".\n\n"
                 "You will only see this message once.");
-    SETTINGS_write_string("last_color_version","3.0.b2.1");
+    SETTINGS_write_string("last_color_version","3.0.b2.2");
   }
 
   if(strcmp(SETTINGS_read_string("last_system_font_version","0.0"),"1.9.21")){
@@ -731,6 +731,14 @@ int radium_main(char *arg){
                 "The default system font has changed. In case you have run Radium before, you might want to go to the Edit menu and select \"Set Default System Font\".\n\n"
                 "You will only see this message once.");
     SETTINGS_write_string("last_system_font_version","1.9.21");
+  }
+
+  if(strcmp(SETTINGS_read_string("last_editor_font_version","0.0"),"3.0.b2.2")){
+    GFX_Message(NULL,
+                "Note!\n\n"
+                "The default editor font has changed. In case you have run Radium before, you might want to go to the Edit menu and select \"Set Default Editor Font\".\n\n"
+                "You will only see this message once.");
+    SETTINGS_write_string("last_editor_font_version","3.0.b2.2");
   }
 
 
