cef57cd34052e5f3041f7ca0327b7ff50a201e0d
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2015-01-15 21:09:21 +0100

    Remove assert. Time can be negative when starting to play.

--- common/PEQcommon.c
+++ common/PEQcommon.c
@@ -99,7 +99,8 @@ static void PC_InsertElement_private(struct PEventQueue *peq, int addplaypos, ST
 	//peq->l.time=addtime + time + (add_latency ? LATENCY : 0); // This didn't work properly I don't quite understand the code.
 	peq->l.time=addtime + time;
 
-        R_ASSERT_RETURN_IF_FALSE(peq->l.time >= 0);
+        // (time can be negative when starting to play.)
+        // R_ASSERT_RETURN_IF_FALSE(peq->l.time >= 0);
 
 	peq->playpos=pc->playpos+addplaypos;
 
