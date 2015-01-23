84f3a1bdda0849aa608f66f19e083181763e491f
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2015-01-23 19:35:23 +0100

    Revert "Fix gc compilation with clang 3.5, and upgrade gc to 7.4.2"
    This reverts commit 8dd322d934109c5512bf6c028d4036a2084e7d3a.

--- build_linux_common.sh
+++ build_linux_common.sh
@@ -27,7 +27,6 @@ export CCC="g++ -mfpmath=sse"
 #export CC="gcc -mfpmath=sse"
 #export CCC="clang++ -stdlib=libc++"
 export CC="clang -Wno-gnu-designator"
-export GCC="gcc -mfpmath=sse"
 export LINKER=g++
 
 export PKG=pkg-config
@@ -55,7 +54,7 @@ export VST_OPTS="-DUSE_VESTIGE=1"
 
 PYTHONLIBPATH=`$PYTHONEXE -c "import sys;print '-L'+sys.prefix+'/lib'"`
 PYTHONLIBNAME=`$PYTHONEXE -c "import sys;print '-lpython'+sys.version[:3]"`
-export OS_LDFLAGS="-llrdf -pthread -lrt -lX11 bin/packages/gc-7.4.2/.libs/libgc.a $PYTHONLIBPATH $PYTHONLIBNAME bin/packages/libgig/src/.libs/libgig.a bin/packages/fluidsynth-1.1.6/src/.libs/libfluidsynth.a `$PKG --libs dbus-1` `$PKG --libs sndfile` `$PKG --libs samplerate` `$PKG --libs glib-2.0` -Wl,-Bstatic -lbfd -Wl,-Bdynamic -lz -liberty -lutil -ldl -ldl bin/packages/libpd-master/libs/libpds.a bin/packages/qhttpserver-master/lib/libqhttpserver.a"
+export OS_LDFLAGS="-llrdf -pthread -lrt -lX11 bin/packages/gc-7.2/.libs/libgc.a $PYTHONLIBPATH $PYTHONLIBNAME bin/packages/libgig/src/.libs/libgig.a bin/packages/fluidsynth-1.1.6/src/.libs/libfluidsynth.a `$PKG --libs dbus-1` `$PKG --libs sndfile` `$PKG --libs samplerate` `$PKG --libs glib-2.0` -Wl,-Bstatic -lbfd -Wl,-Bdynamic -lz -liberty -lutil -ldl -ldl bin/packages/libpd-master/libs/libpds.a bin/packages/qhttpserver-master/lib/libqhttpserver.a"
 
 #  -lz -libery -lintl
 
