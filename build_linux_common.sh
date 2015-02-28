96fca5db27db35ced25175db074bebedd840b38b
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-02-28 17:47:20 +0100

    build fixes

--- build_linux_common.sh
+++ build_linux_common.sh
@@ -35,8 +35,14 @@ export TARGET_OS=linux
 export PKG=pkg-config
 export PYPATH=`$PYTHONEXE -c "import sys;print sys.prefix+'/include/python'+sys.version[:3]"`
 
-export QT_CFLAGS="`$PKG --cflags Qt3Support` -Ibin/packages/qhttpserver-master/src"
+# dynamic Qt4:
+export QT_CFLAGS="`$PKG --cflags Qt3Support --cflags QtOpenGL` -Ibin/packages/qhttpserver-master/src"
 export QT_LDFLAGS="`$PKG --libs Qt3Support --libs QtOpenGL --libs QtNetwork`"
+
+# static Qt4:
+#RQTDIR=/home/kjetil/qt-everywhere-opensource-src-4.8.6
+#export QT_CFLAGS="-DQT_STATIC -DQT3_SUPPORT -I$RQTDIR/include/Qt3Support -I$RQTDIR/include/QtCore -I$RQTDIR/include/QtGui -I$RQTDIR/include/QtNetwork -I$RQTDIR/include/QtSql -I$RQTDIR/include/QtOpenGL -Ibin/packages/qhttpserver-master/src"
+#export QT_LDFLAGS="$RQTDIR/lib/libQt3Support.a $RQTDIR/lib/libQtSql.a $RQTDIR/lib/libQtOpenGL.a $RQTDIR/lib/libQtGui.a $RQTDIR/lib/libQtNetwork.a $RQTDIR/lib/libQtCore.a -lSM -lICE -lfreetype -lfontconfig -lXrender -lpng -lglib-2.0 -lgobject-2.0"
 
 VL_PATH="bin/packages/Visualization-Library-master"
 export VL_CFLAGS="-DVL_STATIC_LINKING -Wall -I$VL_PATH/src -I$VL_PATH/src/3rdparty/Khronos -I$VL_PATH/src/examples"
@@ -57,7 +63,10 @@ export VST_OPTS="-DUSE_VESTIGE=1"
 
 PYTHONLIBPATH=`$PYTHONEXE -c "import sys;print '-L'+sys.prefix+'/lib'"`
 PYTHONLIBNAME=`$PYTHONEXE -c "import sys;print '-lpython'+sys.version[:3]"`
-export OS_LDFLAGS="-llrdf -pthread -lrt -lX11 bin/packages/gc-7.2/.libs/libgc.a $PYTHONLIBPATH $PYTHONLIBNAME bin/packages/libgig/src/.libs/libgig.a bin/packages/fluidsynth-1.1.6/src/.libs/libfluidsynth.a `$PKG --libs dbus-1` `$PKG --libs sndfile` `$PKG --libs samplerate` `$PKG --libs glib-2.0` -Wl,-Bstatic -lbfd -Wl,-Bdynamic -lz -liberty -lutil -ldl -ldl bin/packages/libpd-master/libs/libpds.a bin/packages/qhttpserver-master/lib/libqhttpserver.a pluginhost/Builds/Linux/build/libMyPluginHost.a -lXext"
+
+export OS_LDFLAGS="-llrdf -pthread -lrt -lX11 bin/packages/gc-7.2/.libs/libgc.a $PYTHONLIBPATH $PYTHONLIBNAME bin/packages/libgig/src/.libs/libgig.a bin/packages/fluidsynth-1.1.6/src/.libs/libfluidsynth.a `$PKG --libs dbus-1` `$PKG --libs sndfile` `$PKG --libs samplerate` `$PKG --libs glib-2.0` -Wl,-Bstatic -lbfd -Wl,-Bdynamic -lz -liberty -lutil -ldl -ldl bin/packages/libpd-master/libs/libpds.a pluginhost/Builds/Linux/build/libMyPluginHost.a -lXext"
+
+#bin/packages/qhttpserver-master/lib/libqhttpserver.a
 
 #  -lz -libery -lintl
 
