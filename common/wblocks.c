968be173d1d9b830f5d42992a2f8a679c3eb4924
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2015-03-30 18:06:34 +0200

    Adjust tempocolor area width and linenumber area width somewhat

--- common/wblocks.c
+++ common/wblocks.c
@@ -161,8 +161,8 @@ static void SetLineNumAreaCoordinates(
   wblock->beats_max_num_characters = num_characters_in_num(highest_beat_num);
   wblock->zoomlines_max_num_characters = highest_zoomline_num==-1 ? 0 : num_characters_in_num(highest_zoomline_num+1);
   
-  wblock->beats_x        = wblock->linenumarea.x + wblock->bars_max_num_characters      * window->fontwidth + 2;
-  wblock->zoomlines_x    = wblock->beats_x       + wblock->beats_max_num_characters     * window->fontwidth + 2;
+  wblock->beats_x        = wblock->linenumarea.x + wblock->bars_max_num_characters      * window->fontwidth + 4;
+  wblock->zoomlines_x    = wblock->beats_x       + wblock->beats_max_num_characters     * window->fontwidth + 4;
   wblock->linenumarea.x2 = wblock->zoomlines_x   + wblock->zoomlines_max_num_characters * window->fontwidth;
 
   wblock->linenumarea.width = wblock->linenumarea.x2 - wblock->linenumarea.x;
@@ -280,7 +280,7 @@ void UpdateWBlockWidths(struct Tracker_Windows *window,struct WBlocks *wblock){
 
   //SetZoomLevelAreaWidth(window,wblock);
 
-	wblock->tempocolorarea.width = window->fontwidth*2;
+	wblock->tempocolorarea.width = window->fontwidth*5/2;
 	//wblock->signatureTypearea.width    = 0; //window->fontwidth;
 	wblock->signaturearea.width        = window->fontwidth*GetMaxSignatureWidth(wblock->block);
 	wblock->lpbTypearea.width    = window->fontwidth;
