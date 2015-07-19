71028235ac06d735d73b3e0e8f9b924a5502885d
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-07-19 09:25:16 +0200

    forgot a file

---
+++ common/Semaphores.h
@@ -0,0 +1,66 @@
+#include <QAtomicInt>
+
+namespace cpp11onmulticore{
+  #include "sema.h"
+}
+
+namespace radium{
+
+  
+class Semaphore{
+
+  private:
+    
+    QAtomicInt m_count;
+    cpp11onmulticore::Semaphore m_sema;
+
+  public:
+    
+    Semaphore(int n = 0)
+    {
+      R_ASSERT(n >= 0);
+      m_count = n;
+    }
+
+    int numSignallers(void) {
+      return int(m_count);
+    }
+
+    int numWaiters(void){
+      return -numSignallers();
+    }
+  
+  /*
+    void tryWait(void){
+    }
+  */
+  
+    void wait(void)
+    {
+      if (m_count.fetchAndAddOrdered(-1) <= 0) {
+        m_sema.wait();
+      }
+    }
+
+    void wait(int n){
+      R_ASSERT(n>0);
+      while(--n)
+        wait();
+    }
+  
+    void signal(void)
+    {
+      if (m_count.fetchAndAddOrdered(1) < 0) {
+        m_sema.signal();
+      }
+    }
+
+    void signal(int n){
+      R_ASSERT(n>0);
+      while(--n)
+        signal();
+    }
+
+};
+
+}
