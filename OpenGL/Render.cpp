4ae186cd8273466090e8d2f0b0c39521050b96b3
Kjetil Matheussen <k.s.matheussen@notam02.no>
2016-02-21 12:06:05 +0100

    compilation fix

--- OpenGL/Render.cpp
+++ OpenGL/Render.cpp
@@ -1138,11 +1138,11 @@ static void create_pitches(const struct Tracker_Windows *window, const struct WB
   if (indicator_node == &note->l && indicator_pitch_num!=-1) {
     const vector_t *nodes = get_nodeline_nodes(nodelines, wblock->t.y1);
     
-    if (indicator_pitch_num >= nodes->num_elements)
+    if (indicator_pitch_num >= nodes->num_elements) {
 #if !defined(RELEASE)
       RError("indicator_pitch_node_num(%d) >= nodes->num_elements(%d)",indicator_pitch_num,nodes->num_elements); // Fix: This has happened.
 #endif
-    else {
+    } else {
       //printf("indicator_pitch_num: %d\n",indicator_pitch_num);
       struct Node *node = (struct Node *)nodes->elements[indicator_pitch_num];
       draw_node_indicator(node->x, node->y-wblock->t.y1);
