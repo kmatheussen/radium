fd6487a8fd13f7b77a270dada519868f12cd974c
Kjetil Matheussen <k.s.matheussen@notam02.no>
2016-03-12 14:18:47 +0100

    Disabled red "Rec" update, cause it caused GL_create to be called twice when switching block

--- common/Ptask2Mtask.c
+++ common/Ptask2Mtask.c
@@ -104,7 +104,7 @@ void P2MUpdateSongPosCallBack(void){
     //printf("Aft. w: %d, r: %d\n",window->curr_block,root->curr_block);
   }      
 
-
+#if 0
   // make sure "Rec" is updated
   {
     static struct Tracks *current_track = NULL;
@@ -112,12 +112,14 @@ void P2MUpdateSongPosCallBack(void){
     
     struct Tracks *track = wblock->wtrack->track;
     if (track != current_track || (current_track!=NULL && track->is_recording != current_track_recording)){
+      //printf("   ptask2mtask, calling gl_create %d %d\n",track != current_track,(current_track!=NULL && track->is_recording != current_track_recording));
       current_track = track;
       current_track_recording = current_track->is_recording;
       GL_create(window, wblock);
     }
   }
-
+#endif
+  
   //GE_set_curr_realline(wblock->curr_realline);
   //  printf("till_curr_realline: %d\n",wblock->till_curr_realline);
   //ScrollEditorToRealLine(window,wblock,wblock->curr_realline);
