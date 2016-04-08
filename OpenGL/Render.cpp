6ade3a33c8c678f55cc286076fc93492eef70510
Kjetil Matheussen <k.s.matheussen@notam02.no>
2016-04-08 13:23:41 +0200

    ...or it is the current pianonote

--- OpenGL/Render.cpp
+++ OpenGL/Render.cpp
@@ -1352,7 +1352,7 @@ static void create_pianoroll(const struct Tracker_Windows *window, const struct 
           float x_text = (nodeline->x1 + nodeline->x2) / 2;
           bool text_is_inside = (x_text > wtrack->pianoroll_area.x - 20) && (x_text < wtrack->pianoroll_area.x2 + 20);
             
-          if (text_is_inside) {
+          if (text_is_inside || is_current) {
             draw_pianonote_text(window, notenum, is_current, x_text, nodeline->y1);
 
             // pitch_end
