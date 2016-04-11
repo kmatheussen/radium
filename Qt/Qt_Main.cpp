ce6f4952535f95483f094f8d33a151bcf30f0e36
Kjetil Matheussen <k.s.matheussen@notam02.no>
2016-04-11 15:24:13 +0200

    macosx: Disable fullscreen (since it makes the program crash)

--- Qt/Qt_Main.cpp
+++ Qt/Qt_Main.cpp
@@ -785,6 +785,9 @@ WPoint GetPointerPos(struct Tracker_Windows *tvisual){
 }
 
 void GFX_toggleFullScreen(struct Tracker_Windows *tvisual){
+#ifdef FOR_MACOSX
+  GFX_Message(NULL, "Full screen not supported in OSX");
+#else
   QMainWindow *main_window = (QMainWindow *)tvisual->os_visual.main_window;
 
   if(main_window->isFullScreen()){
@@ -792,6 +795,7 @@ void GFX_toggleFullScreen(struct Tracker_Windows *tvisual){
   }else{
     main_window->showFullScreen();
   }
+#endif
 }
 
 
