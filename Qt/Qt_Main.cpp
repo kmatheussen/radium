eb2273e15a07ec7e53979db4ddcbf2fe37393566
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2016-12-06 18:21:34 +0100

    Don"t show playlist window when starting up

--- Qt/Qt_Main.cpp
+++ Qt/Qt_Main.cpp
@@ -1807,6 +1807,8 @@ int radium_main(char *arg){
 
     }
 
+    GFX_PlayListWindowToBack();
+
     //qapplication->setMainWidget(main_window);
     //GFX_PlayListWindowToFront(); // To provoce setting width to 'blocklist_width'
     main_window->show();
