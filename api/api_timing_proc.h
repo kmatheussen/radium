abe1473e95c327c5bae15f08e6adcc4caa9e28ae
Kjetil Matheussen <k.s.matheussen@notam02.no>
2017-04-02 09:43:31 +0200

    forgotten file

---
+++ api/api_timing_proc.h
@@ -0,0 +1,9 @@
+
+#ifndef _RADIUM_API_API_TIMING_PROC_H
+#define _RADIUM_API_API_TIMING_PROC_H
+
+extern LANGSPEC dyn_t API_getAllBPM(const struct Blocks *block);
+extern LANGSPEC dyn_t API_getAllLPB(const struct Blocks *block);
+extern LANGSPEC dyn_t API_getAllTemponodes(const struct Blocks *blocks);
+
+#endif
