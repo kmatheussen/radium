902af2c4d88753902dd00de869ee3a88ef0deac0
Kjetil Matheussen <k.s.matheussen@notam02.no>
2017-04-23 10:25:31 +0200

    Fix vsync warning message

--- OpenGL/Widget.cpp
+++ OpenGL/Widget.cpp
@@ -1741,19 +1741,21 @@ QWidget *GL_create_widget(QWidget *parent){
   }
 
   if (GL_get_vsync()==false){
-	if (SETTINGS_read_bool("show_vsync_warning_during_startup", true)) {
-	  vector_t v = {};
-	  VECTOR_push_back(&v,"Ok");
-	  int vsync_hide = VECTOR_push_back(&v,"Don't show this message again");
-
-	  int vsyncret = GFX_Message(&v,
-			  "Warning: VSync is disabled. You probably don't want to do that. You can enable vsync under"
-			  "<p>"
-			  "Edit -> Preferences -> OpenGL -> Vertical Blank -> VSync"
-			  "<p>"
-			  "Please only consider turning vsync in radium off if your drivers are misbehaving. There is little performance gain compared to eg. Video Games and so disabling vsync option is provided for compability reasons.");
-	  if (vsyncret==vsync_hide)
-		  SETTINGS_write_bool("show_vsync_warning_during_startup", false);
+    if (SETTINGS_read_bool("show_vsync_warning_during_startup", true)) {
+      vector_t v = {};
+      VECTOR_push_back(&v,"Ok");
+      int vsync_hide = VECTOR_push_back(&v,"Don't show this message again");
+      
+      int vsyncret = GFX_Message(&v,
+                                 "Warning: VSync is disabled. You probably don't want to do that. You can enable vsync under"
+                                 "<p>"
+                                 "Edit -> Preferences -> OpenGL -> Vertical Blank -> VSync"
+                                 "<p>"
+                                 "Disabling vsync will normally cripple graphical performance.<br>"
+                                 "You should only turn off vsync if your gfx driver is misbehaving."
+                                 );
+      if (vsyncret==vsync_hide)
+        SETTINGS_write_bool("show_vsync_warning_during_startup", false);
     }
   }
 
