kmatheussen/radium

commit 93a4b5713aee1fe5b0e4c522cb3fa0f74951d5e0

Audio: Precompute port range hints in ladspa plugins. (less time to set an effect value)

audio/Ladspa_plugins.cpp
@@ -93,6 +93,7 @@ struct TypeData{
   float *max_values;
   bool uses_two_handles;
 
+  LADSPA_PortRangeHintDescriptor *hint_descriptors;
   const char **effect_names;
 };
 }
@@ -308,6 +309,7 @@ static void *create_plugin_data(const SoundPluginType *plugin_type, SoundPlugin 
     }
   }
 
+
   if(descriptor->activate!=NULL){
     descriptor->activate(data->handles[0]);
     if(type_data->uses_two_handles==true)
@@ -408,7 +410,7 @@ static void set_effect_value(SoundPlugin *plugin, int time, int effect_num, floa
 
   if(value_format==PLUGIN_FORMAT_SCALED){
 
-    LADSPA_PortRangeHintDescriptor hints = get_hintdescriptor(type,effect_num);
+    LADSPA_PortRangeHintDescriptor hints = type_data->hint_descriptors[effect_num];
 
     if(LADSPA_IS_HINT_LOGARITHMIC(hints)){
 
@@ -450,7 +452,7 @@ static float get_effect_value(SoundPlugin *plugin, int effect_num, enum ValueFor
 #endif
 
   if(value_format==PLUGIN_FORMAT_SCALED){
-    const LADSPA_PortRangeHintDescriptor hints = get_hintdescriptor(type,effect_num);
+    const LADSPA_PortRangeHintDescriptor hints = type_data->hint_descriptors[effect_num];
 
     if(LADSPA_IS_HINT_LOGARITHMIC(hints)){
 
@@ -477,8 +479,9 @@ static const char *get_effect_name(SoundPlugin *plugin, int effect_num){
 }
 
 static int get_effect_format(SoundPlugin *plugin, int effect_num){
-  const struct SoundPluginType *type = plugin->type;
-  const LADSPA_PortRangeHintDescriptor hints = get_hintdescriptor(type,effect_num);
+  TypeData *type_data = (struct TypeData*)plugin->type->data;
+  
+  const LADSPA_PortRangeHintDescriptor hints = type_data->hint_descriptors[effect_num];
 
   if(LADSPA_IS_HINT_TOGGLED(hints))
     return EFFECT_FORMAT_BOOL;
@@ -651,6 +654,10 @@ static void add_ladspa_plugin_type(const QFileInfo &file_info){
     type_data->min_values     = (float*)V_calloc(sizeof(float),plugin_type->num_effects);
     type_data->default_values = (float*)V_calloc(sizeof(float),plugin_type->num_effects);
     type_data->max_values     = (float*)V_calloc(sizeof(float),plugin_type->num_effects);
+
+    type_data->hint_descriptors = (LADSPA_PortRangeHintDescriptor*)V_calloc(sizeof(LADSPA_PortRangeHintDescriptor), plugin_type->num_effects);
+    for(int i = 0 ; i < plugin_type->num_effects ; i++)
+      type_data->hint_descriptors[i] = get_hintdescriptor(plugin_type,i);
 
     {
       int effect_num = 0;

audio/MultiCore.cpp
@@ -219,8 +219,8 @@ public:
     can_start_main_loop.wait();
 
 #ifdef FOR_MACOSX
-    bool started = false;
-    printf("  Trying to call soundproducer_queue.get();\n");
+    //bool started = false;
+    //printf("  Trying to call soundproducer_queue.get();\n");
 #endif
 
     while(true){
@@ -268,10 +268,10 @@ public:
       
 
 #ifdef FOR_MACOSX
-      if (started==false){
-        printf("  Success. Running Runner thread main loop started.\n");
-        started = true;
-      }
+      //if (started==false){
+      //  printf("  Success. Running Runner thread main loop started.\n");
+      //  started = true;
+      //}
 #endif
 
       if (ATOMIC_GET(must_exit)) {

commit 8c4f8d7e7cb55c35c0ad072e9d568f50e6c84d73

Fix crash introduced in 93a4b5713aee1fe5b0e4c522cb3fa0f74951d5e0

audio/Ladspa_plugins.cpp
@@ -368,9 +368,7 @@ static void buffer_size_is_changed(SoundPlugin *plugin, int new_buffer_size){
   }
 }
 
-static LADSPA_PortRangeHintDescriptor get_hintdescriptor(const SoundPluginType *plugin_type, int effect_num){
-  TypeData *type_data = (TypeData*)plugin_type->data;
-  const LADSPA_Descriptor *descriptor = type_data->descriptor;
+static LADSPA_PortRangeHintDescriptor get_hintdescriptor(const LADSPA_Descriptor *descriptor, int effect_num){
 
   int effect_num2 = 0;
   for(unsigned int portnum=0;portnum<descriptor->PortCount;portnum++){
@@ -385,7 +383,7 @@ static LADSPA_PortRangeHintDescriptor get_hintdescriptor(const SoundPluginType *
     }
   }
 
-  RWarning("Unknown effect %d for Ladspa plugin \"%s\"\n",effect_num,type_data->Name);
+  RWarning("Unknown effect %d for Ladspa plugin \"%s\"\n", effect_num);
   return 0;
 }
 
@@ -634,6 +632,8 @@ static void add_ladspa_plugin_type(const QFileInfo &file_info){
 
     plugin_type->is_instrument = false;
 
+    // Find num_effects, num_inputs, and num_outputs
+    //
     for(unsigned int portnum=0;portnum<descriptor->PortCount;portnum++){
       const LADSPA_PortDescriptor portdescriptor = descriptor->PortDescriptors[portnum];
 
@@ -655,10 +655,19 @@ static void add_ladspa_plugin_type(const QFileInfo &file_info){
     type_data->default_values = (float*)V_calloc(sizeof(float),plugin_type->num_effects);
     type_data->max_values     = (float*)V_calloc(sizeof(float),plugin_type->num_effects);
 
+    // Find type_data->hint_descriptors.
+    // type_data->hint_descriptor maps effect_num to descriptor for effect_num. (speeds up set_effect())
+    //
+    // Note that we are not storing pointers to LADSPA_PortRangeHintDescriptor (which would not work). Instead
+    // we are copying the memory areas of LADSPA_PortRangeHintDescriptor from the dynamically loaded library and into type_data.
+    // (LADSPA_PortRangeHintDescriptor is actually just an integer)
+    //
     type_data->hint_descriptors = (LADSPA_PortRangeHintDescriptor*)V_calloc(sizeof(LADSPA_PortRangeHintDescriptor), plugin_type->num_effects);
     for(int i = 0 ; i < plugin_type->num_effects ; i++)
-      type_data->hint_descriptors[i] = get_hintdescriptor(plugin_type,i);
+      type_data->hint_descriptors[i] = get_hintdescriptor(descriptor,i);
 
+    // Find type_data->min_values and type_data->max_values
+    //
     {
       int effect_num = 0;
       type_data->effect_names = (const char**)V_calloc(sizeof(char*),plugin_type->num_effects);
@@ -1057,7 +1066,7 @@ static void init_menues(){
 
 void create_ladspa_plugins(void){
 #if !defined(RELEASE)
-  return; // takes long time to load ladspa plugins in gdb.
+  //return; // takes long time to load ladspa plugins in gdb.
 #endif
   
   char ladspa_path[1024];