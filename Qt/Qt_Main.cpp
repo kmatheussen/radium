c6f0ed10193d78b5754904298783ac93ce3753df
Kjetil Matheussen <k.s.matheussen@notam02.no>
2017-06-29 17:13:33 +0200

    add link to qtbug ticket for hack in prev. commit

--- Qt/Qt_Main.cpp
+++ Qt/Qt_Main.cpp
@@ -3045,7 +3045,7 @@ int main(int argc, char **argv){
   // Create application here in order to get default style. (not recommended, but can't find another way)
   qapplication=new MyApplication(argc,argv);
   qapplication->setAttribute(Qt::AA_MacDontSwapCtrlAndMeta, true);
-  qapplication->setAttribute(Qt::AA_DontCreateNativeWidgetSiblings); // Fix splitter handlers on OSX. Seems like a good flag to set in general. Seems like a hack qt has added to workaround bugs in qt.
+  qapplication->setAttribute(Qt::AA_DontCreateNativeWidgetSiblings); // Fix splitter handlers on OSX. Seems like a good flag to set in general. Seems like a hack qt has added to workaround bugs in qt. https://bugreports.qt.io/browse/QTBUG-33479
   
   g_startup_rect = QApplication::desktop()->screenGeometry(); // Probably no point. Hoped that it would force radium to open on the same desktop that was current when program started.
 
