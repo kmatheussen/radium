465b599caa7e8441c00a4d451a101dcf538c766d
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2017-07-07 15:05:50 +0200

    Sampler/Fluidsynth: Option to remove bookmark when informed about non-existing bookmarked directory

--- common/settings_proc.h
+++ common/settings_proc.h
@@ -33,6 +33,8 @@ extern LANGSPEC void SETTINGS_write_int(const char *key, int64_t val);
 extern LANGSPEC void SETTINGS_write_double(const char *key, double val);
 extern LANGSPEC void SETTINGS_write_string(const char *key, const char *val);
 
+extern LANGSPEC bool SETTINGS_remove(const char* key);
+
 extern LANGSPEC void SETTINGS_init(void);
 
 #ifdef USE_QT4
