bfe4d7330219e2ae0d8150950ead0ddce441b7bf
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2017-11-14 17:07:49 +0100

    Gui: Let the song properties window and the song comment window have normal check boxes

--- Qt/create_source_from_ui.sh
+++ Qt/create_source_from_ui.sh
@@ -7,6 +7,13 @@ UIC=$1
 MOC=$2
 V=$3
 
+CHECKBOX=MyQCheckBox
+
+if [[ $4 == DONT_USE_RADIUM_CHECKBOXES ]]
+then
+    CHECKBOX=QCheckBox
+fi
+
 # * Creates the .h and .cpp files for the qt designer ui files.
 # * Replace "protected" with "public" in the header files. (to avoid having to make subclasses)
 # * Replace QLineEdit with MyQLineEdit so that we can override focusIn
@@ -14,7 +21,7 @@ V=$3
 UIFILE=qt4_$3.ui
 #UIFILE=$3.ui
 
-$UIC $UIFILE | sed s/protected/public/ | sed s/QComboBox/FocusSnifferQComboBox/| sed s/QLineEdit/FocusSnifferQLineEdit/ | sed s/QTextEdit/FocusSnifferQTextEdit/ | sed s/QListWidget/FocusSnifferQListWidget/ | sed s/QSpinBox/MyQSpinBox/| sed s/QDoubleSpinBox/FocusSnifferQDoubleSpinBox/ | sed s/QTableWidget/FocusSnifferQTableWidget/ | sed s/\ QSlider/\ MyQSlider/ | sed s/\ QCheckBox/\ MyQCheckBox/ | sed s/\ QToolButton/\ MyQButton/ | sed s:\#include\ \<QtGui/FocusSnifferQLineEdit\>://\ \ qlineedit.h:| sed s:\#include\ \<QtWidgets/FocusSnifferQComboBox\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQLineEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/FocusSnifferQTextEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQTextEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/FocusSnifferQListWidget\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQListWidget\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/MyQSpinBox\>://\ \ qspingox.h:| sed s:\#include\ \<QtWidgets/MyQSpinBox\>://\ \ qspingox.h:| sed s:\#include\ \<QtGui/FocusSnifferQDoubleSpinBox\>://\ \ qspingox.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQDoubleSpinBox\>://\ \ qspingox.h: | sed s:\#include\ \<QtGui/FocusSnifferQTableWidget\>://\ \ qspingox.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQTableWidget\>://\ \ qspingox.h: >Qt_$3.h
+$UIC $UIFILE | sed s/protected/public/ | sed s/QComboBox/FocusSnifferQComboBox/| sed s/QLineEdit/FocusSnifferQLineEdit/ | sed s/QTextEdit/FocusSnifferQTextEdit/ | sed s/QListWidget/FocusSnifferQListWidget/ | sed s/QSpinBox/MyQSpinBox/| sed s/QDoubleSpinBox/FocusSnifferQDoubleSpinBox/ | sed s/QTableWidget/FocusSnifferQTableWidget/ | sed s/\ QSlider/\ MyQSlider/ | sed s/\ QCheckBox/\ $CHECKBOX/ | sed s/\ QToolButton/\ MyQButton/ | sed s:\#include\ \<QtGui/FocusSnifferQLineEdit\>://\ \ qlineedit.h:| sed s:\#include\ \<QtWidgets/FocusSnifferQComboBox\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQLineEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/FocusSnifferQTextEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQTextEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/FocusSnifferQListWidget\>://\ \ qlineedit.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQListWidget\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/MyQSpinBox\>://\ \ qspingox.h:| sed s:\#include\ \<QtWidgets/MyQSpinBox\>://\ \ qspingox.h:| sed s:\#include\ \<QtGui/FocusSnifferQDoubleSpinBox\>://\ \ qspingox.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQDoubleSpinBox\>://\ \ qspingox.h: | sed s:\#include\ \<QtGui/FocusSnifferQTableWidget\>://\ \ qspingox.h: | sed s:\#include\ \<QtWidgets/FocusSnifferQTableWidget\>://\ \ qspingox.h: >Qt_$3.h
 
 #$UIC $UIFILE | sed s/protected/public/ | sed s/QSpinBox/MyQSpinBox/ | sed s:\#include\ \<QtGui/FocusSnifferQLineEdit\>://\ \ qlineedit.h: | sed s:\#include\ \<QtGui/MyQSpinBox\>://\ \ qspingox.h: >Qt_$3.h
 
