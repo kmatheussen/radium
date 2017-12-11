859f43d64d38d0822b2f76b95556d1bd31ce5deb
Kjetil Matheussen <k.s.matheussen@notam02.no>
2017-12-11 14:33:27 +0100

    Compilation fix

--- Qt/Qt_Main.cpp
+++ Qt/Qt_Main.cpp
@@ -14,7 +14,12 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
+#define __STDC_FORMAT_MACROS 1
+
 #include "../common/includepython.h"
+
+#include <inttypes.h>
+
 
 #include <boost/version.hpp>
 #if (BOOST_VERSION < 100000) || ((BOOST_VERSION / 100 % 1000) < 63)
