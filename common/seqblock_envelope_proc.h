ee8847389b3be5ee2b2851189966d9772979d1d3
Kjetil Matheussen <k.s.matheussen@usit.uio.no>
2017-12-29 13:06:34 +0100

    Sequencer block volume envelope, step 1

---
+++ common/seqblock_envelope_proc.h
@@ -0,0 +1,42 @@
+#ifndef RADIUM_COMMON_SEQBLOCK_ENVELOPE_PROC_H
+#define RADIUM_COMMON_SEQBLOCK_ENVELOPE_PROC_H
+
+
+
+extern LANGSPEC void RT_SEQBLOCK_ENVELOPE_called_before_scheduler(void);
+extern LANGSPEC void RT_SEQBLOCK_ENVELOPE_called_before_editor(struct SeqTrack *seqtrack);
+extern LANGSPEC void RT_SEQBLOCK_ENVELOPE_called_after_scheduler_and_before_audio(void);
+extern LANGSPEC void RT_SEQBLOCK_ENVELOPE_called_when_player_stopped(void);
+
+#ifdef __cplusplus
+
+struct SeqblockEnvelope *SEQBLOCK_ENVELOPE_create(struct SeqTrack *seqtrack, struct SeqBlock *seqblock, const hash_t *automation_state);
+void SEQBLOCK_ENVELOPE_free(struct SeqblockEnvelope *seqblockenvelope);
+int SEQBLOCK_ENVELOPE_add_automation(struct SeqblockEnvelope *seqblockenvelope, double seqtime1, double value1, int logtype, double seqtime2, double value2);
+int SEQBLOCK_ENVELOPE_get_num_automations(struct SeqblockEnvelope *seqblockenvelope);
+double SEQBLOCK_ENVELOPE_get_value(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+double SEQBLOCK_ENVELOPE_get_seqtime(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+int SEQBLOCK_ENVELOPE_get_logtype(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+int SEQBLOCK_ENVELOPE_get_num_nodes(struct SeqblockEnvelope *seqblockenvelope);
+int SEQBLOCK_ENVELOPE_add_node(struct SeqblockEnvelope *seqblockenvelope, double seqtime, double value, int logtype);
+void SEQBLOCK_ENVELOPE_delete_node(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+void SEQBLOCK_ENVELOPE_set_curr_node(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+void SEQBLOCK_ENVELOPE_cancel_curr_node(struct SeqblockEnvelope *seqblockenvelope);
+void SEQBLOCK_ENVELOPE_set_curr_automation(struct SeqTrack *seqtrack, struct SeqBlock *seqblock);
+
+// May be called if it there is no current automation.
+void SEQBLOCK_ENVELOPE_cancel_curr_automation(void);
+void SEQBLOCK_ENVELOPE_set(struct SeqTrack *seqtrack, struct SeqBlock *seqblock, int nodenum, double seqtime, double value, int lotype);
+
+////////////////////////
+
+void RT_SEQBLOCK_ENVELOPE_called_after_editor_and_before_audio(const struct SeqTrack *seqtrack, int64_t seqblock_time);
+void RT_SEQBLOCK_ENVELOPE_called_when_player_stopped(void);
+hash_t *SEQBLOCK_ENVELOPE_get_state(struct SeqblockEnvelope *seqblockenvelope);
+float SEQBLOCK_ENVELOPE_get_node_x(struct SeqblockEnvelope *seqblockenvelope, int nodenum);
+float SEQBLOCK_ENVELOPE_get_node_y(struct SeqblockEnvelope *seqblockenvelope, int seqtracknum, int nodenum);
+void SEQBLOCK_ENVELOPE_paint(QPainter *p, const struct SeqBlock *seqblock, float x1, float y1, float x2, float y, bool paint_nodes);
+
+#endif
+
+#endif
