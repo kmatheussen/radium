7bc9c53aeed8e537783da0f457c08311314ddd30
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-03-28 12:44:06 +0200

    Compilation fixes

--- api/api_seqtracks.cpp
+++ api/api_seqtracks.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
 #include <inttypes.h>
 
+#include <QSet>
 
 #include "../common/nsmtracker.h"
 #include "../common/seqtrack_proc.h"
