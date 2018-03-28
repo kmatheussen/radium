f49d6b4f12d38a0b34f736f4af488d4c325d7958
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-03-28 12:44:06 +0200

    Compilation fixes

--- common/seqtrack.cpp
+++ common/seqtrack.cpp
@@ -20,6 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include <gc.h>
 
 #include <QFileInfo>
+#include <QSet>
 
 #include "nsmtracker.h"
 #include "player_proc.h"
