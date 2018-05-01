32d08ba76a08b245500996bf707f7afa8cc97d83
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-05-01 21:05:13 +0200

    forgotten file

---
+++ Qt/Qt_sequencer_proc.h
@@ -0,0 +1,11 @@
+
+#ifndef _RADIUM_QT_QT_SEQUENCER_PROC_H
+#define _RADIUM_QT_QT_SEQUENCER_PROC_H
+
+void SEQUENCER_WIDGET_initialize(QWidget *main_window);
+
+void SEQUENCER_WIDGET_call_very_often(void);
+
+QWidget *SEQUENCER_WIDGET_get_widget(void);
+
+#endif
