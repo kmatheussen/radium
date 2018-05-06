f0cc013d373feaa6138e57c878aca19d4f3460b1
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-05-06 18:26:05 +0200

    minor fix for prev. commit

--- api/api_gui.cpp
+++ api/api_gui.cpp
@@ -487,7 +487,7 @@ static QQueue<Gui*> g_delayed_resized_guis; // ~Gui removes itself from this one
       float meter_width = total_meter_space / num_channels;
 
       x1 = start_x + border_width + (ch * (border_width+meter_width));
-      x2 = (ch==num_channels-1) ? _x2 : floor(start_x + border_width + (ch * (border_width+meter_width)) + meter_width);
+      x2 = (ch==num_channels-1) ? _x2 : round(start_x + border_width + (ch * (border_width+meter_width)) + meter_width); // Calling 'round' to get rid of think, almost invisible line, that sometimes can be seen between the channels.
     }
 
     float get_pos_y1(void) const {
