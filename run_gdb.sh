212b40eb7571de3a2573ff2fdd1c19738bec3369
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-05-29 09:02:47 +0200

    build: run_gdb.sh script. Useful when wanting to run RELEASE version under gdb.

---
+++ run_gdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+THIS_DIR=$(dirname $(readlink -f $0))
+XCB_LIB_DIR=$THIS_DIR/bin/packages/libxcb-1.12/src/.libs
+
+if ! file $XCB_LIB_DIR ; then
+    echo "Unable to find directory $XCB_LIB_DIR"
+    exit -1
+fi
+
+export LD_LIBRARY_PATH=$XCB_LIB_DIR${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH G_DEBUG=fatal-criticals gdb --args bin/radium_linux.bin $@; killall -9 radium_progress_window ; killall -9 radium_crashreporter
