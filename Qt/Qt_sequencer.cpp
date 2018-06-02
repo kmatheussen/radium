e20b0dcb58c5c7de9b3c86dccbaf7d3732f40b1a
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-06-02 10:39:44 +0200

    Sequencer: Don"t wrap lines when painting seqblock headers. I.e. if there is not space for all of "Name of seqblock" in the seqblock header, don"t cut at the best space character but display as much as possible.

--- Qt/Qt_sequencer.cpp
+++ Qt/Qt_sequencer.cpp
@@ -197,7 +197,6 @@ static void myDrawText(QPainter &painter, QRectF rect, QString text, QTextOption
   if (is_playing() && pc->playtype==PLAYSONG && smooth_scrolling()){
 
     // Paint glyphs manually. QPainter::drawText doesn't have floating point precision.
-
     int len=text.length();
 
     double x = rect.x();
@@ -217,7 +216,10 @@ static void myDrawText(QPainter &painter, QRectF rect, QString text, QTextOption
       x += g.width;
     }
   } else {
+    
+    option.setWrapMode(QTextOption::NoWrap);
     painter.drawText(rect, text, option);
+    
   } 
 }
 
