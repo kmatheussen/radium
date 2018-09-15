a434788439cf086a921ca50151044eeafd5c5be8
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-09-15 13:09:12 +0200

    fix

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@ set -x
 
 PREFIX=$1
 
-if [ ! -d "$PREFIX" ]; then
-    echo "Directory $PREFIX does not exist"
-    exit -1
-fi
+#if [ ! -d "$PREFIX" ]; then
+#    echo "Directory $PREFIX does not exist"
+#    exit -1
+#fi
 
+mkdir -p "$PREFIX"
 
 TARGET=$1/radium
 
@@ -21,7 +22,7 @@ fi
 
 echo $TARGET
 
-mkdir -p $TARGET
+mkdir -p "$TARGET"
 
 cd bin
 for a in * ; do
@@ -32,14 +33,17 @@ done
 
 mkdir -p "$TARGET/packages"
 
+# s7
 cp -a packages/s7 "$TARGET/packages/"
 rm -f "$TARGET/packages/s7/*.o"
 rm -fr "$TARGET/packages/s7/sndlib"
 
+# faust
 mkdir -p "$TARGET/packages/faust2"
 cp -a packages/faust2/examples "$TARGET/packages/faust2/"
 cp -a packages/faust2/architecture "$TARGET/packages/faust2/"
 
+# pure data
 cp -a packages/libpd-master "$TARGET/packages/"
 cd "$TARGET/packages/libpd-master"
 make clean
