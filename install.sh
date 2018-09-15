c66bedcf015c511ec35d087330f06204ec36a082
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-09-15 11:46:34 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+PREFIX=$1
+
+TARGET=$1/radium
+
+echo $TARGET
+
+mkdir -p $TARGET
+
+cd bin
+for a in * ; do
+    if ! [[ "$a" = "packages" ]]; then
+        cp -a "$a" "$TARGET/"
+    fi
+done
+
+mkdir -p "$TARGET/packages"
+
+cp -a packages/s7 "$TARGET/packages/"
+rm -f "$TARGET/packages/s7/*.o"
+rm -f "$TARGET/packages/s7/sndlib"
+
+mkdir -p "$TARGET/packages/faust2"
+cp -a packages/faust2/examples "$TARGET/packages/faust2/"
+cp -a packages/faust2/architecture "$TARGET/packages/faust2/"
+
+cp -a packages/libpd-master "$TARGET/packages/"
+cd "$TARGET/packages/libpd-master"
+make clean
+rm -f libpds.o
+
+
