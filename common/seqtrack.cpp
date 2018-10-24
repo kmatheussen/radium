078428d0b31322f568e1fb45e28648d839104fb8
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-10-24 18:09:01 +0200

    compilation fix

--- common/seqtrack.cpp
+++ common/seqtrack.cpp
@@ -15,8 +15,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
 
-
+#include <inttypes.h>
 #include <math.h>
+
 #include <gc.h>
 
 #include <QFileInfo>
