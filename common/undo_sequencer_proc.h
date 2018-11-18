a07889ac99fbce242aee6ad6506549e066921427
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-11-18 16:19:09 +0100

    1. Sequencer: Optimized undo handling in various situations. 2. Sequencer: Don"t close audio seqblock gui window when undoing/redoing any of the parameters (some more work remains here). API: undoSeqblock

--- common/undo_sequencer_proc.h
+++ common/undo_sequencer_proc.h
@@ -2,6 +2,7 @@
 #define UNDO_SEQUENCER_PROC_H
 
 extern LANGSPEC void ADD_UNDO_FUNC(Sequencer(void));
+extern LANGSPEC void ADD_UNDO_FUNC(Seqblock(int seqtracknum, int seqblocknum));
 extern LANGSPEC void ADD_UNDO_FUNC(SeqtrackAutomations(void));
 extern LANGSPEC void ADD_UNDO_FUNC(SeqblockAutomation(int automationnum, int seqblocknum, int seqtracknum));
 extern LANGSPEC void ADD_UNDO_FUNC(SeqblockFades(int seqtracknum, int seqblocknum));
