4002bf421a5379d2970d733e0a53b2e90d9690e8
Kjetil Matheussen <k.s.matheussen@notam02.no>
2018-12-16 18:50:46 +0100

    Build: Compile with -flto in release mode

--- Qt/Qt_sequencer.cpp
+++ Qt/Qt_sequencer.cpp
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
 
 #define INCLUDE_SNDFILE_OPEN_FUNCTIONS 1
-#define RADIUM_ACCESS_SEQBLOCK_AUTOMATION 1
 #include "../common/nsmtracker.h"
 
 #include "Qt_MyQCheckBox.h"
@@ -48,7 +47,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include "../audio/Mixer_proc.h"
 
 #include "../audio/Peaks.hpp"
-//#include "../audio/Envelope.hpp"
+#include "../audio/Envelope.hpp"
 #include "../audio/SampleReader_proc.h"
 
 #include "../embedded_scheme/s7extra_proc.h"
