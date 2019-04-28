860446c4703191ea2c917e9fee80e28151f5331f
Kjetil Matheussen <k.s.matheussen@notam02.no>
2019-04-28 10:38:37 +0200

    fix

--- Qt/Qt_Bs_edit_proc.h
+++ Qt/Qt_Bs_edit_proc.h
@@ -1,10 +1,11 @@
 
 #ifndef _RADIUM_QT_QT_BS_EDIT_PROC_H
 #define _RADIUM_QT_QT_BS_EDIT_PROC_H
-class QtWidgets;
+
 
 void set_default_slider_height();
 
+class QtWidget;
 QWidget *BS_get(void);
 
 //void GFX_PlayListWindowToBack(void);
