kmatheussen/radium

commit 6eac4812eb83bdcc4a4d491138a35f52b748a85d

Startup: Check if calling backtrace crashes the program. We don"t get a crashreport if backtrace crashes, so this needs to be tested separately first.

common/Process.hpp
@@ -46,6 +46,8 @@ private:
   
   bool _start_has_been_called = false;
 
+  double _start_time;
+  
   void connect_signals(void){
     {
       IsAlive is_alive(this);
@@ -73,7 +75,11 @@ private:
                            _exit_status = exitStatus;
                            _exit_code = exitCode;
                          }
-                         process->deleteLater();
+
+                         if(get_qprocess() == NULL){
+                           R_ASSERT_NON_RELEASE(false);
+                         }else
+                           process->deleteLater();
                        });
     }
   }
@@ -91,7 +97,9 @@ public:
   
   void start(QString program){
     R_ASSERT_RETURN_IF_FALSE(_start_has_been_called==false);
-    
+
+    R_ASSERT_RETURN_IF_FALSE(get_qprocess() != NULL);
+
     _start_has_been_called = true;
     
 #if defined(FOR_LINUX) || defined(FOR_MACOSX)
@@ -105,6 +113,8 @@ public:
 #else
     _process->start(program);
 #endif
+    
+    _start_time = TIME_get_ms();
   }
 
   enum class Status{
@@ -121,12 +131,41 @@ public:
   bool error_has_occured(void) const {
     return _error_occured;
   }
+
+  double get_running_time(double start_time) const {
+    R_ASSERT_RETURN_IF_FALSE2(_start_has_been_called, -1);
+
+    return TIME_get_ms() - start_time;
+  }
+
+  double get_running_time(void) const {
+    return get_running_time(_start_time);
+  }
+    
+  bool has_timed_out(double timeout, double start_time) const {
+    if (timeout < 0)
+      return false;
+    
+    return get_running_time() >= timeout;
+  }
+
+  bool has_timed_out(double timeout) const {
+    return has_timed_out(timeout, _start_time);
+  }
   
-  Status get_status(void) const {
+  Status get_status(double timeout = -1) {
     if (!_start_has_been_called)
       return Status::NOT_STARTED;
 
+    //printf("   timeout: %d. running_time: %d. Finished? (%d && %d). Error_occured: %d\n", (int)timeout, (int)get_running_time(), timeout >= 0, (get_running_time() >= timeout), _error_occured);
+    if (has_timed_out(timeout)){
+      handle_timedout(timeout, _start_time);
+      if (!_error_occured)
+        _proc_error = QProcess::Timedout;
+    }
+      
     if (_error_occured){
+
       if(_proc_error==QProcess::Crashed)
         return Status::CRASHED;
       else if(_proc_error==QProcess::FailedToStart)
@@ -152,10 +191,10 @@ public:
     return Status::OTHER_ERROR;    
   }
 
-  QString get_status_string(void) const {
+  QString get_status_string(double timeout = -1) {
     R_ASSERT_RETURN_IF_FALSE2(_start_has_been_called==true, "");
     
-    switch(get_status()){
+    switch(get_status(timeout)){
       case Status::NOT_STARTED: return "has not started";
       case Status::RUNNING: return "is running";
       case Status::NOT_FINISHED: return "has not finished";
@@ -170,11 +209,56 @@ public:
     return "(something is wrong)";
   }
 
-  int get_exit_code(void) const{
+  int get_exit_code(void) {
     R_ASSERT(_start_has_been_called==true);
     R_ASSERT(get_status()==Status::FINISHED);
     return _exit_code;
   }
+
+  void kill(void){
+    if (get_qprocess() == NULL)
+      return;
+    
+    if (_process->state() != QProcess::NotRunning)
+      _process->kill();
+  }
+  
+private:
+
+  void handle_timedout(double timeout, double start_time) {
+    R_ASSERT_RETURN_IF_FALSE(get_qprocess() != NULL);
+    
+    if(_has_exited==false){ // Yes, QProcess::waitForFinished returns false also if the program exited normally. (!$%^!#$%!#$%)
+
+      // The errorOccured signal does not seem to be called while calling waitForFinished(), so we have to do this check.
+      if (_error_occured==false){          
+        _error_occured = true;
+        
+        _proc_error = _process->error();
+        
+        //printf("C: %f %f. _proc_error: %d\n",TIME_get_ms(), start_time, _proc_error);
+        
+        if(_proc_error == QProcess::UnknownError){
+
+          if (has_timed_out(timeout, start_time)) {
+            
+            _proc_error = QProcess::Timedout;
+            
+          } else {
+            
+            _proc_error = QProcess::Crashed;
+            
+          }
+          
+          
+        }
+      }
+      
+    }
+      
+  }
+  
+public:
   
   void wait_for_finished(int msecs){
     R_ASSERT_RETURN_IF_FALSE(_start_has_been_called==true);
@@ -185,37 +269,13 @@ public:
       return;
     }
 
-    double start_time = TIME_get_ms();
+    R_ASSERT_RETURN_IF_FALSE(get_qprocess() != NULL);
     
+    double start_time = TIME_get_ms();
+
     if (_process->waitForFinished(msecs)==false){
 
-      if(_has_exited==false){ // Yes, QProcess::waitForFinished returns false also if the program exited normally. (!$%^!#$%!#$%)
-
-        // The errorOccured signal does not seem to be called while calling waitForFinished(), so we have to do this check.
-        if (_error_occured==false){          
-          _error_occured = true;
-          
-          _proc_error = _process->error();
-
-          //printf("C: %f %f. _proc_error: %d\n",TIME_get_ms(), start_time, _proc_error);
-
-          if(_proc_error == QProcess::UnknownError){
-
-            if ((TIME_get_ms() - start_time) >= msecs){
-              
-              _proc_error = QProcess::Timedout;
-              
-            } else {
-
-              _proc_error = QProcess::Crashed;
-              
-            }
-            
-
-          }
-        }
-
-      }
+      handle_timedout(msecs, start_time);
       
       //fprintf(stderr,"5. Waiting. Error: %d. State: %d. has_exited: %d\n", (int)_process->error(), (int)_process->state(), _has_exited);
     }

Makefile.Qt
@@ -264,6 +264,9 @@ OS_LDFLAGS2 ?=
 OS_DEPS ?=
 OS_OBJS ?=
 
+RADIUM_BFD_CFLAGS ?=
+RADIUM_BFD_LDFLAGS ?=
+
 REQTYPE_TYPE = -DUSE_QT_REQTYPE=1 -DUSE_GTK_REQTYPE=0
 MENU_TYPE = -DUSE_QT_MENU=1 -DUSE_GTK_MENU=0
 
@@ -730,7 +733,7 @@ $(RADIUM_BIN): $(OBJ) $(MAKEFILE) bin/packages/fluidsynth-1.1.6/src/.libs/libflu
 
 
 bin/radium_plugin_scanner: $(PLUGINHOSTSCANNERLIB)
-	$(LINKER) $(PLUGINHOSTSCANNERLIB) -o bin/radium_plugin_scanner $(OS_JUCE_LDFLAGS) `$(PKG) --libs freetype2`
+	$(LINKER) $(PLUGINHOSTSCANNERLIB) -o bin/radium_plugin_scanner $(OS_JUCE_LDFLAGS) $(RADIUM_BFD_LDFLAGS) `$(PKG) --libs freetype2`
 
 
 
@@ -1392,7 +1395,7 @@ X11_keyboard.o:  $(BUILD_DEPENDENCIES) X11/X11_keyboard.cpp
 
 $(P)EditorWidget.h: $(BUILD_DEPENDENCIES)
 
-Qt_Main.o:  $(BUILD_DEPENDENCIES) $(P)Qt_Main.cpp $(P)EditorWidget.h $(P)Qt_instruments_proc.h $(QM)QM_MixerWidget.h Qt/helpers.h audio/SampleRecorder_proc.h common/nag.h Qt/KeyboardFocusFrame.hpp
+Qt_Main.o:  $(BUILD_DEPENDENCIES) $(P)Qt_Main.cpp $(P)EditorWidget.h $(P)Qt_instruments_proc.h $(QM)QM_MixerWidget.h Qt/helpers.h audio/SampleRecorder_proc.h common/nag.h Qt/KeyboardFocusFrame.hpp common/Process.hpp
 	$(MOC) Qt/Qt_Main.cpp > Qt/mQt_Main.cpp
 	$(CCC) $(P)Qt_Main.cpp $(QTOPT) $(SNDFILEOPT) $(IS_LINUX_BINARY) -Wno-null-dereference # -Wno-deprecated-declarations -Wno-shadow 
 
@@ -1670,7 +1673,7 @@ bin/radium_check_jack_status: audio/KillJackd.cpp Jack_plugin.o find_jack_librar
 	$(LINKER) `cat buildtype.opt` radium_check_jack_status.o find_jack_library.o $(JACK_LDFLAGS) `$(PKGqt) --libs Qt5Core` $(QT_SYSTEM_CFLAGS) -o bin/radium_check_jack_status $(FPIC)
 	rm radium_check_jack_status.o
 
-bin/radium_check_opengl: OpenGL/CheckOpenGL.cpp
+bin/radium_check_opengl: OpenGL/CheckOpenGL.cpp common/Process.hpp
 	$(CCC) -Wall -Werror OpenGL/CheckOpenGL.cpp $(CPPOPT) $(QT_CFLAGS) $(OS_OPTS) -std=gnu++11 -DCOMPILE_EXECUTABLE -o radium_check_opengl.o
 	$(LINKER) `cat buildtype.opt` radium_check_opengl.o -o bin/radium_check_opengl $(QT_GUI_LDFLAGS) `$(PKGqt) --libs Qt5OpenGL`
 	rm radium_check_opengl.o
@@ -2386,7 +2389,7 @@ audio/system_compressor.cpp: audio/system_compressor.dsp
 	$(FAUST) -I `pwd`/audio -vec -cn Faust_system_compressor audio/system_compressor.dsp -o audio/system_compressor.cpp
 	./fix_faust_code.sh Faust_system_compressor $@
 
-bin/radium_crashreporter: crashreporter/crashreporter.cpp 
+bin/radium_crashreporter: crashreporter/crashreporter.cpp common/Process.hpp
 	@echo
 	@echo "Changed (bin/radium_crashreporter):"
 	@echo $?
@@ -2403,7 +2406,7 @@ ifeq ($(GCC_MAJOR),9)
 	CRASHREPORTER_OPTS += $(WNO_DEPRECATED_COPY)
 endif
 
-crashreporter.o: crashreporter/crashreporter.cpp
+crashreporter.o: crashreporter/crashreporter.cpp common/Process.hpp
 	@echo
 	@echo "Changed (crashreporter.o):"
 	@echo $?

Qt/Qt_Main.cpp
@@ -95,6 +95,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include "../common/gfx_wtrackheaders_proc.h"
 #include "../common/data_as_text_proc.h"
 #include "../common/sequencer_proc.h"
+#include "../common/Process.hpp"
 
 #include "../api/api_proc.h"
 #include "../api/api_gui_proc.h"
@@ -776,7 +777,48 @@ void MOUSE_CYCLE_schedule_unregister_all(void){
   }    
 }
 
+
+namespace{
+
+  static radium::Process g_check_backtrace_process;
+  static bool g_check_backtrace_process_finished = false;
   
+  static void start_check_backtrace_process(void){
+#if FOR_WINDOWS  
+    g_check_backtrace_process.start(OS_get_full_program_file_path("radium_plugin_scanner.exe") + " test_backtrace");
+#else
+    g_check_backtrace_process.start(OS_get_full_program_file_path("radium_plugin_scanner") + " test_backtrace");
+#endif
+  }
+  
+  static void checkup_on_check_backtrace_process(void){
+    if (g_check_backtrace_process_finished)
+      return;
+    
+    radium::Process::Status status = g_check_backtrace_process.get_status(30000);
+
+    //printf("Status: %d. %s\n", (int)status, g_check_backtrace_process.get_status_string().toUtf8().constData());
+    
+    if (status == radium::Process::Status::RUNNING)
+      return;
+    
+    g_check_backtrace_process_finished = true;
+    
+    if (status == radium::Process::Status::FINISHED)
+      return;
+        
+#if defined(FOR_LINUX) && !defined(IS_LINUX_BINARY)
+    SYSTEM_show_error_message(talloc_format("Error: Unable to get backtrace: \"Bactracing %s\".\nPlease check that Radium is compiled and installed properly.", g_check_backtrace_process.get_status_string().toUtf8().constData()));
+#else
+    SYSTEM_show_error_message(talloc_format("Error: Unable to get backtrace: \"Bactracing %s\".\nPlease report this to k.s.matheussen@notam02.no", g_check_backtrace_process.get_status_string().toUtf8().constData()));
+#endif
+
+    g_check_backtrace_process.kill();
+  }
+
+}
+
+
 class MyApplication
   : public QApplication
 #if USE_QT5
@@ -2197,6 +2239,9 @@ protected:
       static_cast<EditorWidget*>(window->os_visual.widget)->callCustomEvent();
     }
 #endif
+
+    if (is_called_every_ms(5000))
+      checkup_on_check_backtrace_process();
   }
 };
 }
@@ -2687,6 +2732,7 @@ void GFX_set_bottom_widget_height(int new_height){
   }
 }
 
+
 //extern void updateAllFonts(QWidget *widget);
 
 static bool g_load_new_song=true;
@@ -3134,6 +3180,8 @@ int radium_main(const char *arg){
   CalledPeriodically periodic_timer;
 
   FOCUSFRAMES_init();
+
+  start_check_backtrace_process();
   
 #if USE_QT_VISUAL
   qapplication->exec();

Qt/Qt_PopupMenu.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
 #ifndef SAFE_POPUP
   #if FOR_LINUX
-    #define SAFE_POPUP 1 // X often freezes if a program crash while a popup menu is open. (at least when running under gdb)
+    #define SAFE_POPUP 1 // X often freezes if a program crashes while running under gdb and a popup menu is open, and other situations where the Qt main loop is blocked from being called.
   #else
     #define SAFE_POPUP 0
   #endif

audio/Juce_plugin_scanner.cpp
@@ -188,6 +188,12 @@ int main(int argc, char **argv){
   juce::MemoryOutputStream b(1024);
   
   //show_alert("testing alerting");
+
+  if (argc == 2 && !strcmp(argv[1], "test_backtrace")){
+    juce::SystemStats::getStackBacktrace();
+    //juce::Thread::sleep(1000*100);
+    return 0;
+  }
   
   if (argc != 3){
     show_alert(juce::String("Wrong number of arguments. Expected 2, found ") + juce::String::formatted("%d.", argc-1));

audio/Juce_plugins.cpp
@@ -2325,14 +2325,18 @@ static juce::String get_container_descriptions_filename(const wchar_t *container
   return juce::String(OS_get_dot_radium_path(true)) + OS_get_directory_separator() + SCANNED_PLUGINS_DIRNAME + OS_get_directory_separator() + "v2_PluginDescription_" + encoded;
 }
 
-
-static enum PopulateContainerResult launch_program_calling_write_container_descriptions_to_cache_on_disk(const wchar_t *container_filename){
+static juce::String get_plugin_scanner_executable_filename(void){
 #if FOR_WINDOWS
   juce::String executable = juce::String(OS_get_full_program_file_path(STRING_create("radium_plugin_scanner.exe"))); // Don't need to surround string with " and " with juce. You need to do that in Qt.
 #else
   juce::String executable = juce::String(OS_get_full_program_file_path(STRING_create("radium_plugin_scanner")));
 #endif
-  
+  return executable;
+}
+
+static enum PopulateContainerResult launch_program_calling_write_container_descriptions_to_cache_on_disk(const wchar_t *container_filename){
+  juce::String executable = get_plugin_scanner_executable_filename();
+    
   juce::StringArray args;
   args.add(executable);
   args.add(juce::Base64::toBase64(juce::String(container_filename)));

commit 178f91fb63bdbbf8edca2a54d700435bf7e97f5b

Fix radium::Process, broken in 6eac4812eb83bdcc4a4d491138a35f52b748a85d

common/Process.hpp
@@ -65,21 +65,29 @@ private:
     {      
       IsAlive is_alive(this);
       QProcess *process = _process.data();
+      IsAlive process_is_alive(process);
       
-      QObject::connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), [is_alive, this, process](int exitCode, QProcess::ExitStatus exitStatus)
+      QObject::connect(process, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), [is_alive, this, process, process_is_alive](int exitCode, QProcess::ExitStatus exitStatus)
                        {
                          //fprintf(stderr,"       FINISHED: %d %d\n\n\n\n", exitCode, (int)exitStatus);
-                         
+
                          if(is_alive){
                            _has_exited = true;
                            _exit_status = exitStatus;
                            _exit_code = exitCode;
-                         }
+                           if (_exit_status==QProcess::CrashExit)
+                             handle_error(-1, _start_time);
 
-                         if(get_qprocess() == NULL){
-                           R_ASSERT_NON_RELEASE(false);
-                         }else
+                           if(get_qprocess() == NULL){
+                             R_ASSERT_NON_RELEASE(false);
+                           }
+                         }
+                           
+                         if (process_is_alive)
                            process->deleteLater();
+                         else{
+                           R_ASSERT_NON_RELEASE(false);
+                         }                                                    
                        });
     }
   }
@@ -152,43 +160,49 @@ public:
   bool has_timed_out(double timeout) const {
     return has_timed_out(timeout, _start_time);
   }
+
+private:
+
+  Status get_error_status(void) const {
+    R_ASSERT(_error_occured);
+    
+    if(_proc_error==QProcess::Crashed)
+      return Status::CRASHED;
+    else if(_proc_error==QProcess::FailedToStart)
+      return Status::FAILED_TO_START;
+    else if(_proc_error==QProcess::Timedout)
+      return Status::TIMED_OUT;
+    else
+      return Status::OTHER_ERROR;
+  }
+  
+public:
   
   Status get_status(double timeout = -1) {
     if (!_start_has_been_called)
       return Status::NOT_STARTED;
 
+    if (_error_occured)
+      return get_error_status();
+
+    if (_has_exited){
+      R_ASSERT_RETURN_IF_FALSE2(_exit_status==QProcess::NormalExit, Status::OTHER_ERROR);
+      return Status::FINISHED;
+    }
+    
     //printf("   timeout: %d. running_time: %d. Finished? (%d && %d). Error_occured: %d\n", (int)timeout, (int)get_running_time(), timeout >= 0, (get_running_time() >= timeout), _error_occured);
     if (has_timed_out(timeout)){
       handle_timedout(timeout, _start_time);
-      if (!_error_occured)
+      if (!_error_occured){
         _proc_error = QProcess::Timedout;
+        _error_occured = true;
+      }
+
+      if (_error_occured)
+        return get_error_status();
     }
       
-    if (_error_occured){
-
-      if(_proc_error==QProcess::Crashed)
-        return Status::CRASHED;
-      else if(_proc_error==QProcess::FailedToStart)
-        return Status::FAILED_TO_START;
-      else if(_proc_error==QProcess::Timedout)
-        return Status::TIMED_OUT;
-      else
-        return Status::OTHER_ERROR;
-    }
-
-    if (_has_exited==false)
-      return Status::RUNNING;
-
-    if (_exit_status==QProcess::NormalExit)      
-      return Status::FINISHED;
-    
-    if (_exit_status==QProcess::CrashExit) {      
-      R_ASSERT_NON_RELEASE(false); // _error_occured should be true.
-      return Status::CRASHED;      
-    }
-
-    R_ASSERT_NON_RELEASE(false); // _error_occured should be true.
-    return Status::OTHER_ERROR;    
+    return Status::RUNNING;
   }
 
   QString get_status_string(double timeout = -1) {
@@ -225,33 +239,43 @@ public:
   
 private:
 
+  void handle_error(double timeout, double start_time) {
+    R_ASSERT_RETURN_IF_FALSE(get_qprocess() != NULL);
+    R_ASSERT(_error_occured==false);
+    
+    _error_occured = true;
+    
+    _proc_error = _process->error();
+    
+    //printf("C: %f %f. _proc_error: %d\n",TIME_get_ms(), start_time, _proc_error);
+    
+    if(_proc_error == QProcess::UnknownError){
+      
+      if (has_timed_out(timeout, start_time)) {
+        
+        _proc_error = QProcess::Timedout;
+        
+      } else {
+        
+        _proc_error = QProcess::Crashed;
+        
+      }
+      
+      
+    }
+
+  }
+  
   void handle_timedout(double timeout, double start_time) {
     R_ASSERT_RETURN_IF_FALSE(get_qprocess() != NULL);
     
     if(_has_exited==false){ // Yes, QProcess::waitForFinished returns false also if the program exited normally. (!$%^!#$%!#$%)
 
       // The errorOccured signal does not seem to be called while calling waitForFinished(), so we have to do this check.
-      if (_error_occured==false){          
-        _error_occured = true;
-        
-        _proc_error = _process->error();
-        
-        //printf("C: %f %f. _proc_error: %d\n",TIME_get_ms(), start_time, _proc_error);
-        
-        if(_proc_error == QProcess::UnknownError){
+      if (_error_occured==false){
 
-          if (has_timed_out(timeout, start_time)) {
-            
-            _proc_error = QProcess::Timedout;
-            
-          } else {
-            
-            _proc_error = QProcess::Crashed;
-            
-          }
-          
-          
-        }
+        handle_error(timeout, start_time);
+        
       }
       
     }