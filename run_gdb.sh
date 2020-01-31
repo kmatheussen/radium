886d7187992e42de379caa3f9ef98327cf5912ce
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-01-31 14:27:06 +0100

    Build: Move setting *SAN_OPTIONS environment variables from make_and_run_linux.sh to run_gdb.sh

--- run_gdb.sh
+++ run_gdb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export LSAN_OPTIONS=suppressions=`pwd`/SanitizerSuppr.txt
+export ASAN_OPTIONS="detect_leaks=0,allocator_may_return_null=1,abort_on_error=1,new_delete_type_mismatch=0,alloc_dealloc_mismatch=0,max_malloc_fill_size=1048576" # new_delete_type_mismatch=0 because of qt5. alloc_dealloc_mismatch because of various vst plugins. detect_stack_use_after_return=1 sounds nice, but is very slow.
+export UBSAN_OPTIONS="print_stacktrace=1:abort_on_error=1"
+#suppressions=`pwd`/SanitizerSuppr.txt:
+export TSAN_OPTIONS="history_size=7,second_deadlock_stack=1,suppressions=SanitizerSuppr.txt"
+
+
 THIS_DIR=$(dirname $(readlink -f $0))
 XCB_LIB_DIR=$THIS_DIR/bin/packages/libxcb-1.13/src/.libs
 
