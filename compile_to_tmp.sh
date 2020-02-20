3446027c9ced6dd5b4dbbc5ff8a18fb76206decf
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-02-20 10:01:40 +0100

    missing file

---
+++ compile_to_tmp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p /tmp/radium_objects
+
+for var in "$@"; do
+    if [ -z ${var##*.c} ]; then
+        base=${var##*/}
+        base=${base%.*}
+        #echo "YES1: $var"
+        break
+    fi
+    if [ -z ${var##*.cpp} ]; then
+        base=${var##*/}
+        base=${base%.*}
+        #echo "YES2: $var"
+        break
+    fi
+done
+
+#echo "base:" $base
+#echo "command:" $@ -o /tmp/$base.o
+
+GREEN='\033[0;32m'
+LIGHT_CYAN='\033[0;36m'
+NC='\033[0m'
+printf "${GREEN}Compiling${LIGHT_CYAN} $base${NC}\n"
+
+"$@" -o /tmp/radium_objects/$base.o
