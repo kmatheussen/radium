9fcb49db76924e4cf563bc9dca17236d56e69b4d
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-03-28 11:09:45 +0100

    Build: Display total compilation time when finished

--- build_linux_qt_visual.sh
+++ build_linux_qt_visual.sh
@@ -7,7 +7,22 @@ export VISUAL="-DUSE_QT_VISUAL=1 -DUSE_GTK_VISUAL=0"
 export GTK_CFLAGS=""
 export GTK_LDFLAGS=""
 
+GREEN='\033[0;32m'
+LIGHT_CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+printf "${GREEN}Compiling${LIGHT_CYAN} Radium${NC}...\n"
+
+start=`date +%s`
+
 ./build_linux_common.sh $@
+
+end=`date +%s`
+
+runtime=$((end-start))
+
+printf "          ${LIGHT_CYAN}Radium ${GREEN} compiled.${YELLOW} (${runtime}s)${NC}\n"
+
 
 #echo "Building finished."
 #echo
