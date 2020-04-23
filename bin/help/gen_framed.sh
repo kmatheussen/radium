14e8819fb88d6046d6ddba7ac67cd7002c059757
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-04-23 11:33:38 +0200

    Help: 1. Help page for pianoroll (not finished). 2. Only show editor sub menues if showing an editor page.

--- bin/help/gen_framed.sh
+++ bin/help/gen_framed.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+endswith(){
+    case $2 in *"$1") true;; *) false;; esac;
+}
+
 cat frame_template1.html >$1_framed.html
 
 cat $1.html | while IFS= read line; do
@@ -9,10 +13,23 @@ cat $1.html | while IFS= read line; do
 done
 
 cat frame_template2.html | while IFS= read line; do
+    
     if [[ $line == *maybeactive* ]]  && [[ $line == *\"$1_framed.html\"* ]] ; then
         echo "$line" | sed s/maybeactive/active/ >>$1_framed.html
+
+    elif [[ $line == *maybeul* ]] ; then
+
+        name=`echo $line | sed '/.*\"\(.*\)\".*/ s//\1/g'`
+
+        if endswith $name $1_framed ; then
+            echo "<ul>" >>$1_framed.html
+        else
+            echo "<ul hidden>" >>$1_framed.html
+        fi
+        
     else
         echo "$line" >>$1_framed.html
     fi
+    
 done
 
