b54f2a7e07616138c101fdb0d9a4a72db3f52cb5
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-04-25 13:11:29 +0200

    fix

--- api/api_requesters.cpp
+++ api/api_requesters.cpp
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include <QFont>
 #include <QApplication>
 
+#include <QSet> // needed to get ProtectedS7FuncVector
 
 #include "../common/nsmtracker.h"
 
