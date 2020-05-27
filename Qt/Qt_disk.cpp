0b2d374d87ae3213f078ba2f8561dec449787994
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-05-27 13:23:19 +0200

    Build: gcc 10 fixes

--- Qt/Qt_disk.cpp
+++ Qt/Qt_disk.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include <QSaveFile>
 #include <QDir>
 #include <QDateTime>
+#include <QSet>
 
 #include "../common/nsmtracker.h"
 #include "../common/visual_proc.h"
