d0e9c3aeb4ee24e77e208f7adda780bb5a3b68e7
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-08-21 15:25:02 +0200

    Instruments: hide lock-checkbox

--- Qt/Qt_patch_widget_callbacks.h
+++ Qt/Qt_patch_widget_callbacks.h
@@ -366,6 +366,8 @@ class Patch_widget : public QWidget, public GL_PauseCaller, public Ui::Patch_wid
     header->setMinimumHeight(header_height);
     header->setMaximumHeight(header_height);
 
+    locked_instrument->hide();
+    
     locked_instrument->setMinimumWidth(header_height);
     locked_instrument->setMaximumWidth(header_height);
     
