2e170f56f8f4490625343b0816495a28945ed1b2
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-12-22 12:19:05 +0100

    packages: improve build.sh

--- bin/packages/build.sh
+++ bin/packages/build.sh
@@ -89,62 +89,70 @@ fi
 #cd ..
 
 
+build_faust() {
+    rm -fr faust
+    tar xvzf faust-master-dev-2019-12-22.tar.gz 
+    mv faust-master-dev faust
+    cd faust
+    tar xvzf ../faustlibraries-2019-12-22.tar.gz
+    rm -fr libraries
+    mv faustlibraries-master libraries
+    patch -p0 <../faust.patch
+    patch -p1 <../faust_setlocale.patch
+    patch -p0 <../faust_svgfix.patch
+    patch -p1 <../faust_HTTPDServer.patch 
+    if env |grep INCLUDE_FAUSTDEV_BUT_NOT_LLVM ; then
+        patch -p0 <../faust_nollvm.patch
+    fi
+    VERBOSE=1 CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" CMAKEOPT="-DCMAKE_BUILD_TYPE=Release -DSELF_CONTAINED_LIBRARY=on -DCMAKE_CXX_COMPILER=`which $DASCXX` -DCMAKE_C_COMPILER=`which $DASCC`" make most
+    cd ..
+}
 
-rm -fr faust
-tar xvzf faust-master-dev-2019-12-22.tar.gz 
-mv faust-master-dev faust
-cd faust
-tar xvzf ../faustlibraries-2019-12-22.tar.gz
-rm -fr libraries
-mv faustlibraries-master libraries
-patch -p0 <../faust.patch
-patch -p1 <../faust_setlocale.patch
-patch -p0 <../faust_svgfix.patch
-patch -p1 <../faust_HTTPDServer.patch 
-if env |grep INCLUDE_FAUSTDEV_BUT_NOT_LLVM ; then
-    patch -p0 <../faust_nollvm.patch
-fi
-VERBOSE=1 CFLAGS="$CFLAGS" CXXFLAGS="$CXXFLAGS" CMAKEOPT="-DCMAKE_BUILD_TYPE=Release -DSELF_CONTAINED_LIBRARY=on -DCMAKE_CXX_COMPILER=`which $DASCXX` -DCMAKE_C_COMPILER=`which $DASCC`" make most
-cd ..
+    
+build_Visualization-Library() {
 
+    rm -fr Visualization-Library-master
+    tar xvzf Visualization-Library-master.tar.gz 
+    cd Visualization-Library-master/
+    patch -p1 <../visualization.patch
+    #sed -i s/"VL_ACTOR_USER_DATA 0"/"VL_ACTOR_USER_DATA 1"/ src/vlCore/config.hpp
+    export MYFLAGS="-std=gnu++11 $CPPFLAGS -fPIC -g  -Wno-c++11-narrowing" #  -D_GLIBCXX_USE_CXX11_ABI=0
+    MYFLAGS="-std=gnu++11 $CPPFLAGS -fPIC -g -Wno-c++11-narrowing" #  -D_GLIBCXX_USE_CXX11_ABI=0
+    echo 'set(CMAKE_CXX_FLAGS "$MYFLAGS")' >>CMakeLists.txt
+    # previously used build type: RelWithDebInfo. Unfortunately, this one enable _DEBUG and various runtime checks.
 
+    #CFLAGS="$CPPFLAGS -fPIC -g" CPPFLAGS="$MYFLAGS" CC="clang" CXX="clang++ $MYFLAGS" cmake -DCMAKE_CXX_FLAGS="$MYFLAGS" CMAKE_CXX_COMPILER="clang++ $MYFLAGS" -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON SUPPORT=ON -DVL_DYNAMIC_LINKING=OFF -DVL_IO_2D_PNG=OFF -DVL_IO_2D_TIFF=OFF -DVL_IO_2D_JPG=OFF -DVL_IO_2D_TGA=OFF -DVL_IO_2D_BMP=OFF .
+    
+    CFLAGS="$CPPFLAGS -fPIC -g" CPPFLAGS="$MYFLAGS" CC="$DASCC" CXX="$DASCXX $MYFLAGS" cmake -DCMAKE_CXX_FLAGS="$MYFLAGS" CMAKE_CXX_COMPILER="$DASCXX $MYFLAGS" -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON SUPPORT=ON -DVL_DYNAMIC_LINKING=OFF -DVL_IO_2D_PNG=OFF -DVL_IO_2D_TIFF=OFF -DVL_IO_2D_JPG=OFF -DVL_IO_2D_TGA=OFF -DVL_IO_2D_BMP=OFF .
+    
+    VERBOSE=1 make -j`nproc`
+    cd ..
+}
 
+build_libpds() {
 
-rm -fr Visualization-Library-master
-tar xvzf Visualization-Library-master.tar.gz 
-cd Visualization-Library-master/
-patch -p1 <../visualization.patch
-#sed -i s/"VL_ACTOR_USER_DATA 0"/"VL_ACTOR_USER_DATA 1"/ src/vlCore/config.hpp
-export MYFLAGS="-std=gnu++11 $CPPFLAGS -fPIC -g  -Wno-c++11-narrowing" #  -D_GLIBCXX_USE_CXX11_ABI=0
-MYFLAGS="-std=gnu++11 $CPPFLAGS -fPIC -g -Wno-c++11-narrowing" #  -D_GLIBCXX_USE_CXX11_ABI=0
-echo 'set(CMAKE_CXX_FLAGS "$MYFLAGS")' >>CMakeLists.txt
-# previously used build type: RelWithDebInfo. Unfortunately, this one enable _DEBUG and various runtime checks.
+    rm -fr libpd-master
+    tar xvzf libpd-master.tar.gz
+    cd libpd-master/
+    sed -i '/define CFLAGS/ s|")| -I/usr/include/tirpc ")|' make.scm
+    sed -i 's/k_cext$//' make.scm
+    sed -i 's/oscx //' make.scm
+    sed -i 's/gcc -O3/gcc -fcommon -O3/' make.scm
+    make clean
+    make -j`nproc`
+    cd ..
+}
 
-#CFLAGS="$CPPFLAGS -fPIC -g" CPPFLAGS="$MYFLAGS" CC="clang" CXX="clang++ $MYFLAGS" cmake -DCMAKE_CXX_FLAGS="$MYFLAGS" CMAKE_CXX_COMPILER="clang++ $MYFLAGS" -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON SUPPORT=ON -DVL_DYNAMIC_LINKING=OFF -DVL_IO_2D_PNG=OFF -DVL_IO_2D_TIFF=OFF -DVL_IO_2D_JPG=OFF -DVL_IO_2D_TGA=OFF -DVL_IO_2D_BMP=OFF .
+build_qhttpserver() {
 
-CFLAGS="$CPPFLAGS -fPIC -g" CPPFLAGS="$MYFLAGS" CC="$DASCC" CXX="$DASCXX $MYFLAGS" cmake -DCMAKE_CXX_FLAGS="$MYFLAGS" CMAKE_CXX_COMPILER="$DASCXX $MYFLAGS" -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON SUPPORT=ON -DVL_DYNAMIC_LINKING=OFF -DVL_IO_2D_PNG=OFF -DVL_IO_2D_TIFF=OFF -DVL_IO_2D_JPG=OFF -DVL_IO_2D_TGA=OFF -DVL_IO_2D_BMP=OFF .
-
-VERBOSE=1 make -j`nproc`
-cd ..
-
-rm -fr libpd-master
-tar xvzf libpd-master.tar.gz
-cd libpd-master/
-sed -i '/define CFLAGS/ s|")| -I/usr/include/tirpc ")|' make.scm
-sed -i 's/k_cext$//' make.scm
-sed -i 's/oscx //' make.scm
-sed -i 's/gcc -O3/gcc -fcommon -O3/' make.scm
-make clean
-make -j`nproc`
-cd ..
-
-rm -fr qhttpserver-master
-tar xvzf qhttpserver-master.tar.gz
-cd qhttpserver-master/
-echo "CONFIG += staticlib" >> src/src.pro
-`../../../find_moc_and_uic_paths.sh qmake`
-make -j`nproc` # necessary to create the moc files.
-cd ..
+    rm -fr qhttpserver-master
+    tar xvzf qhttpserver-master.tar.gz
+    cd qhttpserver-master/
+    echo "CONFIG += staticlib" >> src/src.pro
+    `../../../find_moc_and_uic_paths.sh qmake`
+    make -j`nproc` # necessary to create the moc files.
+    cd ..
+}
 
 
 #rm -fr sndlib
@@ -155,48 +163,58 @@ cd ..
 
 
 #http://www.hpl.hp.com/personal/Hans_Boehm/gc/
-rm -fr gc-7.4.16 libatomic_ops-7.4.14
-tar xvzf gc-7.4.16.tar.gz
-tar xvzf libatomic_ops-7.4.14.tar.gz
-cd gc-7.4.16
-ln -s ../libatomic_ops-7.4.14 libatomic_ops
-#echo 'void RADIUM_ensure_bin_packages_gc_is_used(void){ABORT("GC not configured properly");}' >>malloc.c
-echo 'void RADIUM_ensure_bin_packages_gc_is_used(void){}' >>malloc.c
-echo '#if defined(GC_ASSERTIONS) || !defined(NO_DEBUGGING)' >>malloc.c
-echo "#error "nope"" >>malloc.c
-echo "#endif" >>malloc.c
-#patch -p1 <../gcdiff.patch
-CFLAGS="-mtune=generic -msse2 -mfpmath=sse -g -O2" ./configure --enable-static --disable-shared --disable-gc-debug --disable-gc-assertions
-CFLAGS="-mtune=generic -msse2 -mfpmath=sse -g -O2" make -j`nproc`
-cd ..
+build_gc() {
+    rm -fr gc-7.4.16 libatomic_ops-7.4.14
+    tar xvzf gc-7.4.16.tar.gz
+    tar xvzf libatomic_ops-7.4.14.tar.gz
+    cd gc-7.4.16
+    ln -s ../libatomic_ops-7.4.14 libatomic_ops
+    #echo 'void RADIUM_ensure_bin_packages_gc_is_used(void){ABORT("GC not configured properly");}' >>malloc.c
+    echo 'void RADIUM_ensure_bin_packages_gc_is_used(void){}' >>malloc.c
+    echo '#if defined(GC_ASSERTIONS) || !defined(NO_DEBUGGING)' >>malloc.c
+    echo "#error "nope"" >>malloc.c
+    echo "#endif" >>malloc.c
+    #patch -p1 <../gcdiff.patch
+    CFLAGS="-mtune=generic -msse2 -mfpmath=sse -g -O2" ./configure --enable-static --disable-shared --disable-gc-debug --disable-gc-assertions
+    CFLAGS="-mtune=generic -msse2 -mfpmath=sse -g -O2" make -j`nproc`
+    cd ..
+}
 
-rm -fr fluidsynth-1.1.6
-tar xvzf fluidsynth-1.1.6.tar.gz
-cd fluidsynth-1.1.6
-make clean
-CFLAGS="-fPIC -fno-strict-aliasing -O3 -DDEFAULT_SOUNDFONT=\\\"\\\"" CPPFLAGS="-fPIC -fno-strict-aliasing -O3" CXXFLAGS="-fPIC -fno-strict-aliasing -O3" ./configure --enable-static --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-libsndfile-support --disable-portaudio-support --disable-oss-support --disable-midishare --disable-jack-support --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --disable-aufile-support --disable-dbus-support --without-readline
-# --enable-debug
-make -j`nproc`
-cd ..
+build_fluidsynth() {
 
-rm -fr libgig
-tar xvzf libgig.tar.gz
-cd libgig
-make clean
-CFLAGS="-O3 -fno-strict-aliasing" CPPFLAGS="-O3 -fno-strict-aliasing" CXXFLAGS="-O3 -fno-strict-aliasing" CC=$DASCC CXX=$DASCXX ./configure
-CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" make -j`nproc`
-cd ..
+    rm -fr fluidsynth-1.1.6
+    tar xvzf fluidsynth-1.1.6.tar.gz
+    cd fluidsynth-1.1.6
+    make clean
+    CFLAGS="-fPIC -fno-strict-aliasing -O3 -DDEFAULT_SOUNDFONT=\\\"\\\"" CPPFLAGS="-fPIC -fno-strict-aliasing -O3" CXXFLAGS="-fPIC -fno-strict-aliasing -O3" ./configure --enable-static --disable-aufile-support --disable-pulse-support --disable-alsa-support --disable-libsndfile-support --disable-portaudio-support --disable-oss-support --disable-midishare --disable-jack-support --disable-coreaudio --disable-coremidi --disable-dart --disable-lash --disable-ladcca --disable-aufile-support --disable-dbus-support --without-readline
+    # --enable-debug
+    make -j`nproc`
+    cd ..
+}
+
+build_libgig () {
+
+    rm -fr libgig
+    tar xvzf libgig.tar.gz
+    cd libgig
+    make clean
+    CFLAGS="-O3 -fno-strict-aliasing" CPPFLAGS="-O3 -fno-strict-aliasing" CXXFLAGS="-O3 -fno-strict-aliasing" CC=$DASCC CXX=$DASCXX ./configure
+    CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" make -j`nproc`
+    cd ..
+}
 
 
-# QScintilla
-rm -fr QScintilla_gpl-2.10.8
-tar xvzf QScintilla_gpl-2.10.8.tar.gz
-cd QScintilla_gpl-2.10.8/Qt4Qt5/
-echo "CONFIG += staticlib" >> qscintilla.pro
-`../../../../find_moc_and_uic_paths.sh qmake`
-patch -p0 <../../qscintilla.patch
-make -j`nproc`
-cd ../..
+build_qscintilla() {
+    # QScintilla
+    rm -fr QScintilla_gpl-2.10.8
+    tar xvzf QScintilla_gpl-2.10.8.tar.gz
+    cd QScintilla_gpl-2.10.8/Qt4Qt5/
+    echo "CONFIG += staticlib" >> qscintilla.pro
+    `../../../../find_moc_and_uic_paths.sh qmake`
+    patch -p0 <../../qscintilla.patch
+    make -j`nproc`
+    cd ../..
+}
 
 
 #if [[ $RADIUM_QT_VERSION == 5 ]]
@@ -209,28 +227,41 @@ cd ../..
 #    cd ../
 #fi
 
-if [[ $RADIUM_QT_VERSION == 5 && $RADIUM_BUILD_LIBXCB != 0 ]]
-then
-
-    rm -fr xcb-proto-1.13/
-    tar xvjf xcb-proto-1.13.tar.bz2
-    cd xcb-proto-1.13/
-    mkdir install
-    ./configure --prefix=`pwd`/install PYTHON=`which python2`
-    make -j`proc`
-    make install
-    cd ..
+build_xcb() {
     
-    rm -fr libxcb-1.13
-    tar xvjf libxcb-1.13.tar.bz2 
-    cd libxcb-1.13
-    #patch -p1 <../libxcb-1.12.patch
-    export PKG_CONFIG_PATH=`pwd`/../xcb-proto-1.13/install/lib/pkgconfig:$PKG_CONFIG_PATH
-    CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" ./configure PYTHON=`which python2`
-    CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" make -j`nproc`
-    cd ..
-    
-fi
+    if [[ $RADIUM_QT_VERSION == 5 && $RADIUM_BUILD_LIBXCB != 0 ]]
+    then
 
+        rm -fr xcb-proto-1.13/
+        tar xvjf xcb-proto-1.13.tar.bz2
+        cd xcb-proto-1.13/
+        mkdir install
+        ./configure --prefix=`pwd`/install PYTHON=`which python2`
+        make -j`proc`
+        make install
+        cd ..
+        
+        rm -fr libxcb-1.13
+        tar xvjf libxcb-1.13.tar.bz2 
+        cd libxcb-1.13
+        #patch -p1 <../libxcb-1.12.patch
+        export PKG_CONFIG_PATH=`pwd`/../xcb-proto-1.13/install/lib/pkgconfig:$PKG_CONFIG_PATH
+        CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" ./configure PYTHON=`which python2`
+        CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" CPPFLAGS="$CXXFLAGS" make -j`nproc`
+        cd ..
+    
+    fi
+}
+
+
+build_faust
+build_Visualization-Library
+build_libpds
+build_qhttpserver
+build_gc
+build_fluidsynth
+build_libgig
+build_qscintilla
+build_xcb
 
 touch deletemetorebuild
