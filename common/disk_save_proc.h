c079145d9fae8c4a29f9b46f824f6f982757e742
Kjetil Matheussen <k.s.matheussen@notam02.no>
2020-12-31 11:27:04 +0100

    Disk: Automatically add ".rad" suffix when saving song if not already added. Fixes #1332

--- common/disk_save_proc.h
+++ common/disk_save_proc.h
@@ -2,7 +2,7 @@
 extern LANGSPEC bool Save_Initialize(const filepath_t filename, const char *type);
 extern LANGSPEC void Save_Clean(const filepath_t filename,struct Root *theroot, bool is_backup); // Sets dc.success to false if it fails.
 extern LANGSPEC bool Save(struct Root *theroot);
-extern LANGSPEC bool SaveAs2(const filepath_t filename, struct Root *theroot);
+//extern LANGSPEC bool SaveAs2(const filepath_t filename, struct Root *theroot);
 extern LANGSPEC bool SaveAs(struct Root *theroot); // Opens file requester, and then calls SaveAs2.
 extern LANGSPEC bool SaveWithEmbeddedSamples(filepath_t filename, struct Root *theroot);
 extern LANGSPEC bool SaveWithoutEmbeddedSamples(filepath_t filename, struct Root *theroot);
