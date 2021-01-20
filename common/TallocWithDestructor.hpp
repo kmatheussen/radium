eeddef4b6e098460a8f72139f8cb35a53301795d
Kjetil Matheussen <k.s.matheussen@notam02.no>
2021-01-20 13:47:51 +0100

    Internal: Fix possible memory corruption caused by using deleted vptr for subclasses of GC_able. At least it caused asan hits in debug-mode.

--- common/TallocWithDestructor.hpp
+++ common/TallocWithDestructor.hpp
@@ -57,7 +57,7 @@ namespace radium{
     
     // Destructor must actually be virtual for destructors in sub classes to be called (stupid c++).
     virtual ~GC_able(){
-      /* printf("~GC_able called\n"); */
+      //printf("~GC_able called %p\n", this);
       R_ASSERT(_destructor_has_run==false);
       _destructor_has_run=true;
     }
@@ -72,26 +72,40 @@ namespace radium{
       return ret;
     }
 
+#if 0  // Commented out since vptr is deleted by the destructor.
 #if !defined(RELEASE)
     bool _allowed_to_call_destructor = false;
 #endif
+#endif
     
-    // Should never be deleted explicitly though.
+    // Need to override the delete operator so that the memory block is not released by the system (BDW-GC takes care of reusing the memory).
     void operator delete(void *p) {
+      //printf("delete GC_able called %p\n", p);
+      
+      /* 
+
+     *** Block commented out since vptr is deleted by the destructor, so the code below is invalid because it access the object. ****
+
       radium::GC_able *gc_able = static_cast<radium::GC_able*>(p);
 
+      printf("..delete GC_able called %p %p\n", p, gc_able);
+            
       R_ASSERT_RETURN_IF_FALSE(gc_able==dynamic_cast<radium::GC_able*>(gc_able));
+
+      printf("....delete GC_able called %p %p %p\n", p, gc_able, dynamic_cast<radium::GC_able*>(gc_able));
 
       R_ASSERT_NON_RELEASE(gc_able->_destructor_has_run==false);
 
 #if !defined(RELEASE)  
-      R_ASSERT_RETURN_IF_FALSE(gc_able->_allowed_to_call_destructor);
-      gc_able->_allowed_to_call_destructor = false;
+      //R_ASSERT_RETURN_IF_FALSE(gc_able->_allowed_to_call_destructor);
+      //gc_able->_allowed_to_call_destructor = false;
       
       //printf("     CUSTOM DELETE: %d\n", g_num_gcable--);
       //abort();
 #endif
+      */
     }
+
   };
   
 
