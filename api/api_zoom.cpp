33832e741516ee1ebd98ecabd93db477555a2b5b
Kjetil Matheussen <k.s.matheussen@notam02.no>
2021-02-21 15:24:54 +0100

    Editor: Make sure cursor is visible after zooming

--- api/api_zoom.cpp
+++ api/api_zoom.cpp
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 #include "../common/OS_visual_input.h"
 #include "../common/visual_proc.h"
 #include "../common/sequencer_proc.h"
+#include "../common/cursor_proc.h"
 
 #include "../Qt/KeyboardFocusFrame.hpp"
 #include "../Qt/Qt_sequencer_proc.h"
