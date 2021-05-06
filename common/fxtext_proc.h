0d6d3f9fe903741eee005f3a60b4fee8c816f426
Kjetil Matheussen <k.s.matheussen@notam02.no>
2021-05-06 14:41:55 +0200

    fix

--- common/fxtext_proc.h
+++ common/fxtext_proc.h
@@ -5,15 +5,17 @@
 // Returns a pointer to AN ARRAY of vectors (one vector for each realline), not a pointer to a vector (as one would think).
 //extern LANGSPEC vector_t *FXTEXTS_get(const struct WBlocks *wblock, const struct WTracks *wtrack, const struct FXs *fxs);
 
-#include "FX.hpp"
+extern LANGSPEC bool FXTEXT_keypress(struct Tracker_Windows *window, struct WBlocks *wblock, struct WTracks *wtrack, int realline, const Place *place, int key);
 
+#ifdef __cplusplus
+
+#include "FX.hpp"
 
 #if USE_QT4
 const FXText_trss FXTEXTS_get(const struct WBlocks *wblock, const struct WTracks *wtrack, const struct FXs *fxs);
 #endif
 
 extern int FXTEXT_subsubtrack(const struct Tracker_Windows *window, const struct WTracks *wtrack, struct FXs **to_fxs);
-extern bool FXTEXT_keypress(struct Tracker_Windows *window, struct WBlocks *wblock, struct WTracks *wtrack, int realline, const Place *place, int key);
 
 static inline int FXTEXT_num(struct Tracks *track){
   int ret = 0;
@@ -36,3 +38,5 @@ static inline bool FXTEXT_has(struct Tracks *track){
   
   return false;
 }
+
+#endif
