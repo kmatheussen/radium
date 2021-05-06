3424c0cca029649a21e7784e89161076a0382c07
Kjetil Matheussen <k.s.matheussen@notam02.no>
2021-05-06 14:47:44 +0200

    grep_touch_files.sh

---
+++ grep_touch_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+git grep "$1" | while read -r line ; do
+    echo `awk -F: '{print $1}'`
+done
+
+git grep "$1" | while read -r line ; do
+    #cut -d: -f1 $line
+    touch `awk -F: '{print $1}'`
+done
