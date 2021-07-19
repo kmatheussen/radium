5052e4f25b09710c87668b614194578a9c20c0ce
Kjetil Matheussen <k.s.matheussen@notam02.no>
2021-07-19 11:23:41 +0200

    Revert prev. commit (the file requester part). It caused sorting by date and size to fail, and sorting only seems to fail when running in wine.

--- Qt/FileRequester.hpp
+++ Qt/FileRequester.hpp
@@ -34,7 +34,7 @@ namespace radium{
         return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
       }
 
-#if !defined(FOR_WINDOWS)
+#if 1 // !defined(FOR_WINDOWS) (seems to fail in Wine only)
       void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) override{
         QFileSystemModel* fileModel = qobject_cast<QFileSystemModel*>(sourceModel());
         fileModel->sort(column, order);
