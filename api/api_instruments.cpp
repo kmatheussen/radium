kmatheussen/radium

commit 165006569a38ad743891aabc1c4f3f8edb8c2de1

Audio: New song properties option to mute output to the 5 system buses when a plugin is bypassed. By default this option is ON for new songs and OFF for old songs (to keep kompatibility). API: setMuteSystemBusesWhenBypassed, muteSystemBusesWhenBypassed

api/api_instruments.cpp
@@ -4161,6 +4161,17 @@ bool includePanAndDryInWetSignal(void){
   return root->song->include_pan_and_dry_in_wet_signal;
 }
 
+void setMuteSystemBusesWhenBypassed(bool val){
+  if(root->song->mute_system_buses_when_bypassed != val){
+    radium::PlayerLock lock;
+    root->song->mute_system_buses_when_bypassed = val;
+  }
+}
+  
+bool muteSystemBusesWhenBypassed(void){
+  return root->song->mute_system_buses_when_bypassed;
+}
+
 static bool g_enable_sample_seek_by_default = true;
 
 bool enableSampleSeekByDefault(void){

api/protos.conf
@@ -1590,6 +1590,8 @@ void setMixerStripCommentsVisible | bool val
 bool mixerStripCommentsVisible
 void setIncludePanAndDryInWetSignal | bool val
 bool includePanAndDryInWetSignal
+void setMuteSystemBusesWhenBypassed | bool val
+bool muteSystemBusesWhenBypassed
 
 bool enableSampleSeekByDefault
 void setEnableSampleSeekByDefault | bool doit

audio/SoundProducer.cpp
@@ -394,13 +394,16 @@ public:
   }
 
   float RT_get_total_link_volume(void) const {
-    const SoundPlugin *source_plugin = SP_get_plugin(source);
-    
     if (!RT_is_enabled)
       return 0.0f;
     
+    const SoundPlugin *source_plugin = SP_get_plugin(source);
+    
     if (_is_bus_link){
-      
+
+      if (root->song->mute_system_buses_when_bypassed && !ATOMIC_GET(source_plugin->effects_are_on))
+        return 0.0f;
+        
       return source_plugin->bus_volume[_bus_num];
       
     } else {

Qt/Qt_audio_instrument_widget_callbacks.h
@@ -876,6 +876,14 @@ public:
       }
       
       drywet_slider->setEnabled(effects_are_on);
+
+      if (root->song->mute_system_buses_when_bypassed) {
+        bus1_widget->setEnabled(effects_are_on);
+        bus2_widget->setEnabled(effects_are_on);
+        bus3_widget->setEnabled(effects_are_on);
+        bus4_widget->setEnabled(effects_are_on);
+        bus5_widget->setEnabled(effects_are_on);
+      }
     }
 
     _comp_widget->update_gui();

Qt/Qt_song_properties_callbacks.h
@@ -90,6 +90,7 @@ class song_properties : public RememberGeometryQDialog, public Ui::Song_properti
     num_bus_ch->setValue(song->default_num_bus_channels);
     
     include_pan_and_dry_in_wet->setChecked(includePanAndDryInWetSignal());
+    mute_system_buses_when_bypassed->setChecked(muteSystemBusesWhenBypassed());
 
     mute_plugin_MIDI->setChecked(song->RT_mute_plugin_MIDI_when_muted);
     send_plugin_MIDI_through->setChecked(song->RT_send_plugin_MIDI_through_when_bypassed);
@@ -246,6 +247,13 @@ public slots:
       return;
 
     setIncludePanAndDryInWetSignal(val);
+  }
+  
+  void on_mute_system_buses_when_bypassed_toggled(bool val){
+    if (_initing==true)
+      return;
+
+    setMuteSystemBusesWhenBypassed(val);
   }
   
   void on_mute_automation_toggled(bool val){

Qt/qt4_song_properties.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>881</width>
-    <height>477</height>
+    <height>491</height>
    </rect>
   </property>
   <property name="sizePolicy">
@@ -224,26 +224,6 @@
       <string>Mixer</string>
      </property>
      <layout class="QGridLayout" name="gridLayout_2">
-      <item row="2" column="0">
-       <widget class="QCheckBox" name="include_pan_and_dry_in_wet">
-        <property name="text">
-         <string>Include Panning and Width in the Wet signal</string>
-        </property>
-       </widget>
-      </item>
-      <item row="3" column="0">
-       <spacer name="verticalSpacer">
-        <property name="orientation">
-         <enum>Qt::Vertical</enum>
-        </property>
-        <property name="sizeHint" stdset="0">
-         <size>
-          <width>20</width>
-          <height>40</height>
-         </size>
-        </property>
-       </spacer>
-      </item>
       <item row="1" column="0">
        <layout class="QHBoxLayout" name="horizontalLayout">
         <item>
@@ -271,10 +251,37 @@
         </item>
        </layout>
       </item>
+      <item row="4" column="0">
+       <spacer name="verticalSpacer">
+        <property name="orientation">
+         <enum>Qt::Vertical</enum>
+        </property>
+        <property name="sizeHint" stdset="0">
+         <size>
+          <width>20</width>
+          <height>40</height>
+         </size>
+        </property>
+       </spacer>
+      </item>
       <item row="0" column="0">
        <widget class="QCheckBox" name="mixer_comments_visible">
         <property name="text">
          <string>Show comments</string>
+        </property>
+       </widget>
+      </item>
+      <item row="2" column="0">
+       <widget class="QCheckBox" name="include_pan_and_dry_in_wet">
+        <property name="text">
+         <string>Include Panning and Width in the Wet signal</string>
+        </property>
+       </widget>
+      </item>
+      <item row="3" column="0">
+       <widget class="QCheckBox" name="mute_system_buses_when_bypassed">
+        <property name="text">
+         <string>Mute output to the 5 system buses when a plugin is bypassed</string>
         </property>
        </widget>
       </item>

common/disk_song.cpp
@@ -69,6 +69,7 @@ DC_start("SONG");
 
         DC_SSB("mixer_comments_visible", song->mixer_comments_visible);
         DC_SSB("include_pan_and_dry_in_wet_signal", song->include_pan_and_dry_in_wet_signal);
+        DC_SSB("mute_system_buses_when_bypassed", song->mute_system_buses_when_bypassed);
         DC_SSB("mute_editor_automation_when_track_is_muted", song->mute_editor_automation_when_track_is_muted);
         //DC_SSB("use_sequencer_timing", song->use_sequencer_tempos_and_signatures); // saved in sequencer state instead.
 
@@ -115,7 +116,7 @@ struct Song *LoadSong(void){
                 "SEQUENCER",
                 "COMMENT"
 	};
-	const char *vars[17]={
+	const char *vars[18]={
 		"num_blocks",
 		"length",
 		"songname",
@@ -129,6 +130,7 @@ struct Song *LoadSong(void){
                 "mixer_comments_visible",
                 "mute_editor_automation_when_track_is_muted",
                 "include_pan_and_dry_in_wet_signal",
+                "mute_system_buses_when_bypassed",
                 "mute_plugin_MIDI_when_muted",
                 "send_plugin_MIDI_through_when_bypassed",
                 "implicitly_mute_plugin_MIDI",
@@ -139,6 +141,7 @@ struct Song *LoadSong(void){
 
         song->mute_editor_automation_when_track_is_muted = false; // Compatibility with older songs.
         song->include_pan_and_dry_in_wet_signal = false; // Compatibility with older songs.
+        song->mute_system_buses_when_bypassed = false; // Compatibility with older songs.
         song->use_swinging_beats_in_sequencer = true;
         song->display_swinging_beats_in_seqblocks_in_sequencer = true;
 
@@ -169,7 +172,7 @@ struct Song *LoadSong(void){
 
         COMMENT_reset();
 
-        GENERAL_LOAD(7,17)
+        GENERAL_LOAD(7,18)
 
 obj0:
 	DC_ListAdd1(&song->tracker_windows,LoadWindow());
@@ -262,22 +265,25 @@ var12:
         goto start;
 
 var13:
-        song->RT_mute_plugin_MIDI_when_muted = DC_LoadB();
+        song->mute_system_buses_when_bypassed = DC_LoadB();
         goto start;
 
 var14:
-        song->RT_send_plugin_MIDI_through_when_bypassed = DC_LoadB();
+        song->RT_mute_plugin_MIDI_when_muted = DC_LoadB();
         goto start;
 
 var15:
+        song->RT_send_plugin_MIDI_through_when_bypassed = DC_LoadB();
+        goto start;
+
+var16:
         song->RT_implicitly_mute_plugin_MIDI = DC_LoadB();
         goto start;        
         
-var16:
+var17:
         song->default_num_bus_channels = DC_LoadI();
         BUS_set_num_channels(song->default_num_bus_channels);
         goto start;
-var17:
 var18:
 var19:
 var20:

commit 8ab244871f0487c84db2efeeb303b154de68b98a

Mixer: Update visibility for implicitly muted connection caused by bypass in 165006569a38ad743891aabc1c4f3f8edb8c2de1

api/api_instruments.cpp
@@ -2540,9 +2540,39 @@ float getAudioConnectionGain(instrument_t source_id, instrument_t dest_id, bool 
     return 0.0;
 }
 
+bool getConnectionImplicitlyDisabled(instrument_t source_id, instrument_t dest_id){
+  if (!root->song->mute_system_buses_when_bypassed)
+    return false;
+  
+  struct Patch *patch_source = getAudioPatchFromNum(source_id);
+  if(patch_source==NULL)
+    return true;
+
+  struct SoundPlugin *plugin_source = (struct SoundPlugin*)patch_source->patchdata;
+  if (plugin_source==NULL){
+    handleError("getConnectionImplicitlyDisabled: Source instrument #%d has been closed", (int)source_id.id);
+    return true;
+  }
+
+  if (!is_bypassed(plugin_source))
+    return false;
+  
+  struct Patch *patch_dest = getAudioPatchFromNum(dest_id);
+  if(patch_dest==NULL)
+    return true;
+
+  struct SoundPlugin *plugin_dest = (struct SoundPlugin*)patch_dest->patchdata;
+  if (plugin_dest==NULL){
+    handleError("getNumInputChannels: Destination instrument #%d has been closed", (int)dest_id.id);
+    return true;
+  }
+
+  return PLUGIN_get_bus_num(plugin_dest->type) >= 0;
+}
+
 bool getConnectionEnabled(instrument_t source_id, instrument_t dest_id, bool show_error_if_not_connected){
   bool ret;
-  if (get_connection_gain_enabled("getConnectioEnabled", source_id, dest_id, NULL, &ret, show_error_if_not_connected))
+  if (get_connection_gain_enabled("getConnectionEnabled", source_id, dest_id, NULL, &ret, show_error_if_not_connected))
     return ret;
   else
     return false;

api/protos.conf
@@ -1457,6 +1457,8 @@ bool changeAudioConnections | dynvec_t changes
 bool hasAudioConnection | instrument_t source_id | instrument_t dest_id
 bool hasEventConnection | instrument_t source_id | instrument_t dest_id
 
+bool getConnectionImplicitlyDisabled | instrument_t source_id | instrument_t dest_id # returns true if source is bypassed and dest is a system bus.
+
 bool getConnectionEnabled | instrument_t source_id | instrument_t dest_id | bool show_error_if_not_connected ? true
 void setConnectionEnabled | instrument_t source_id | instrument_t dest_id | bool is_enabled | bool redraw_mixer_strips ? true # Redraw all mixer strips since there might be more than one mixer strips window visible. Note: setting an event connection to false currently doesn't do anything.
 void undoConnectionEnabled | instrument_t source_id | instrument_t dest_id

audio/SoundPlugin.cpp
@@ -1860,6 +1860,7 @@ static void PLUGIN_set_effect_value2(struct SoundPlugin *plugin, const int time,
       
     case EFFNUM_EFFECTS_ONOFF:
       SET_SMOOTH_ON_OFF(&plugin->drywet, plugin->effects_are_on, value, plugin->last_written_effect_values_native[plugin->type->num_effects + EFFNUM_DRYWET], 0.0f);
+      RT_schedule_mixer_strips_redraw();
       break;
 
       

audio/SoundProducer.cpp
@@ -365,6 +365,7 @@ public:
         return 0;
       }
 #endif
+
       const SoundPluginType *type = plugin->type;      
       int effect_num = type->num_effects + EFFNUM_BUS1 + _bus_num;
       
@@ -401,7 +402,7 @@ public:
     
     if (_is_bus_link){
 
-      if (root->song->mute_system_buses_when_bypassed && !ATOMIC_GET(source_plugin->effects_are_on))
+      if (root->song->mute_system_buses_when_bypassed && is_bypassed(source_plugin))
         return 0.0f;
         
       return source_plugin->bus_volume[_bus_num];
@@ -2282,7 +2283,7 @@ public:
       
       // Input peaks
       if (_num_dry_sounds > 0){
-        bool do_bypass = !ATOMIC_GET(_plugin->effects_are_on); //_plugin->drywet.smoothing_is_necessary==false && _plugin->drywet.value==0.0f;
+        bool do_bypass = is_bypassed(_plugin); //_plugin->drywet.smoothing_is_necessary==false && _plugin->drywet.value==0.0f;
         
         float input_peaks[_num_dry_sounds];
         for(int ch=0;ch<_num_dry_sounds;ch++) {

bin/scheme/mixer-strips.scm
@@ -1018,7 +1018,8 @@
   (define (is-enabled?)
     ;;(c-display "INSTRUMENT:" instrument-name)
     (if is-send?
-        (<ra> :get-connection-enabled parent-instrument-id instrument-id #f) ;; #f means don"t show error if not connected. That might happen if redrawing before remaking after changing configuration.
+        (and (not (<ra> :get-connection-implicitly-disabled parent-instrument-id instrument-id))
+             (<ra> :get-connection-enabled parent-instrument-id instrument-id #f)) ;; #f means don"t show error if not connected. That might happen if redrawing before remaking after changing configuration.
         (>= (<ra> :get-instrument-effect instrument-id "System Effects On/Off") 0.5)))
   
   (define (is-grayed?)

mixergui/QM_MixerWidget.cpp
@@ -2481,9 +2481,11 @@ namespace{
                   if (chip->_last_updated_bypass != is_bypass){
                     chip->_last_updated_bypass = is_bypass;
                     chip->update();
+                    for(auto connection : chip->_output_audio_connections)
+                      connection->update_shape(true, true); // Update implitly disabled connection caused by bypass.
                   }
                   
-                  if (chip->_last_updated_recording != is_recording){
+                  if (chip->_last_updated_recording != is_recording) {
                     chip->_last_updated_recording = is_recording;
                     chip->update();
                   }

mixergui/QM_chip.cpp
@@ -1668,7 +1668,18 @@ static bool connection_is_implicitly_muted(SuperConnection *connection){
   if (connection->to==NULL || connection->from==NULL)
     return false;
 
-  return SP_get_link_implicitly_muted(connection->to->_sound_producer, connection->from->_sound_producer, NULL);
+  if (SP_get_link_implicitly_muted(connection->to->_sound_producer, connection->from->_sound_producer, NULL))
+    return true;
+  
+  if (!root->song->mute_system_buses_when_bypassed)
+    return false;
+
+  SoundPlugin *from_plugin = SP_get_plugin(connection->from->_sound_producer);
+  
+  if (!is_bypassed(from_plugin))
+    return false;
+  
+  return SP_get_bus_num(connection->to->_sound_producer) >= 0;
 }
 
 static float get_connection_color_alpha(SuperConnection *connection){