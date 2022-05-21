3222ab4e3e03a5e5792a09468ed516f633f7591c
akimaze <akimazemusic@gmail.com>
2022-05-21 18:12:44 +0200

    Added getPresedBrowserFrame().

--- Qt/Qt_PresetBrowser.h
+++ Qt/Qt_PresetBrowser.h
@@ -21,5 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 class QWidget;
 QWidget *createPresetBrowserWidget(void);
 QWidget *getPresetBrowserWidget(void);
+QWidget *getPresetBrowserWidgetFrame(void);
 
 #endif
