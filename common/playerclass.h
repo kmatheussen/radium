f93fd11a2d8e656145266cb2a1b567b884f23b91
Kjetil Matheussen <k.s.matheussen@notam02.no>
2022-09-10 11:23:13 +0200

    player: Remove PLAYBLOCK_NONLOOP type. It complicated the code unnecessarily. Followup to #1388

--- common/playerclass.h
+++ common/playerclass.h
@@ -156,7 +156,6 @@ extern PlayerClass *pc;
 #define PLAYSONG 0
 #define PLAYBLOCK 1
 #define PLAYRANGE 2 // Must never be set. We set pc->is_playing_range to true instead.
-#define PLAYBLOCK_NONLOOP 3
 
 extern int g_assert_not_stopping_player;
 extern RSemaphore *g_player_stopped_semaphore;
