b09195f034bc3ae52eb16bf1238d0d47c5208ac7
Kjetil Matheussen <k.s.matheussen@notam02.no>
2024-11-02 14:55:44 +0100

    Build: Use included python2

--- find_python_path.sh
+++ find_python_path.sh
@@ -2,6 +2,9 @@
 
 #source $(dirname "${0}")/bash_setup.sh
 
+echo $(readlink -f $(dirname "${0}"))/bin/packages/python27_install/bin/python
+exit 0
+
 if which python2 >/dev/null 2>/dev/null ; then
     which python2
 elif which python >/dev/null 2>/dev/null ; then
