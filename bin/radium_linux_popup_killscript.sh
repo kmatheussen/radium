5d546833c7fe2fd8feecbad5061fe74313499166
Kjetil Matheussen <k.s.matheussen@notam02.no>
2026-03-01 00:21:10 +0100

    Linux/Dev: Option to not kill radium in case a popup menu has been visible too long (to avoid locking up computer during development)

--- bin/radium_linux_popup_killscript.sh
+++ bin/radium_linux_popup_killscript.sh
@@ -6,10 +6,26 @@
 echo $$ >/tmp/$1
 
 sleep $2
+#sleep 2
 
 if ps -Af|grep $3|grep radium_linux.bin ; then
 
     if [ -f /tmp/$1 ] ; then
+		statusfile=$(mktemp)
+		
+		xterm -e 'dialog --timeout 10 --title "Kill Radium?" --backtitle "A menu has been open for $2 seconds. Locked?" --yesno "Press Yes to kill radium. (Will kill radium in 10 seconds if no response)." 7 60 ; echo $? > '$statusfile
+
+		response=$(cat $statusfile)
+		rm $statusfile
+
+		echo "response: $response"
+		
+		case $response in
+			1)
+				echo "Pressed no"
+				exit
+		esac
+
         echo "killing it: $1 $2 $3"
         kill -9 $3
     fi
