9d7c159bd00f62aea44a134ebf459582435e64e1
Kjetil Matheussen <k.s.matheussen@notam02.no>
2026-07-27 19:31:06 +0200

    Preferences: 1. Add ability to switch between many color configurations. 2. Add a "snapshot" functionality to support undoing current color or all colors back to latest snapshot. 3. Add buttons to set current or all colors to default. 4. Remove the "Save permanent" button from the Colors tab, instead automatically save. 5. Make it much faster to change colors which also changes style sheet.

--- api/api_requesters.cpp
+++ api/api_requesters.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 
 #include <QFont>
 #include <QApplication>
+#include <QDir>
 
 #include <QSet> // needed to get ProtectedS7FuncVector
 
