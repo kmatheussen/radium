724bfc921a5f3425101b8f60e98ef50f1b187f66
Kjetil Matheussen <k.s.matheussen@notam02.no>
2026-08-22 13:23:53 +0200

    Build fix

--- build_linux_common.sh
+++ build_linux_common.sh
@@ -384,7 +384,7 @@ do_source_sanity_checks() {
             exit -1
 	fi            
 	
-	if git grep -e if\( --or -e if\ \( *|grep \=|grep -v \=\=|grep -v \!\=|grep -v \>\=|grep -v \<\=|grep -v pluginhost|grep -v bin/scheme|grep -v rtmidi|grep -v python|grep -v amiga|grep -v unused_files|grep -v weakjack|grep -v radium_wrap_1.c|grep -v keybindings.conf |grep -v bin/help | grep -v Visualization-Library-master; then
+	if git grep -e if\( --or -e if\ \( *|grep \=|grep -v \=\=|grep -v \!\=|grep -v \>\=|grep -v \<\=|grep -v pluginhost|grep -v bin/scheme|grep -v rtmidi|grep -v python|grep -v amiga|grep -v unused_files|grep -v weakjack|grep -v radium_wrap_1.c|grep -v keybindings.conf |grep -v bin/help | grep -v Visualization-Library-master|grep -v "\[NO_IF_=_WARNING\]" |grep -v cloudflare ; then
             echo
             echo "ERROR in line(s) above. A single '=' can not be placed on the same line as an if.";
             echo
